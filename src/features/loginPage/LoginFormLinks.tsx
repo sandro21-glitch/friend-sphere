bc3dfcd480b801878b11284cea4917b157497de2
Sandro <vipsandro11@gmail.com>
2024-06-15 23:54:50 +0400

    update LoginFormLinks.tsx

--- src/features/loginPage/LoginFormLinks.tsx
+++ src/features/loginPage/LoginFormLinks.tsx
@@ -4,12 +4,12 @@ const LoginFormLinks = () => {
   return (
     <div className="flex items-center justify-center mb-5">
       <Link
-        to=""
-        className="block w-1/3 text-center pb-2 border-b-2 border-deep-blue"
+        to="/signin"
+        className="block w-1/3 text-center pb-2 border-b-2 border-azure-blue"
       >
         Sign In
       </Link>
-      <Link to="" className="block w-1/3 text-center pb-2 border-b">
+      <Link to="/signup" className="block w-1/3 text-center pb-2 border-b">
         Sign Up
       </Link>
     </div>
