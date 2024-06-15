d0e50d0bac071052fc71c345588a92651c1f33d8
Sandro <vipsandro11@gmail.com>
2024-06-15 23:55:03 +0400

    feat: create SignUpBtn.tsx

---
+++ src/features/registerPage/SignUpBtn.tsx
@@ -0,0 +1,9 @@
+const SignUpBtn = () => {
+  return (
+    <button className="bg-azure-blue hover:bg-deep-blue transition-all ease-in duration-200 w-full text-white py-2 rounded-lg mb-3">
+      Sign Up
+    </button>
+  );
+};
+
+export default SignUpBtn;
