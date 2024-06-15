10a8baa888cbdcd7927439d81ec574c363681835
Sandro <vipsandro11@gmail.com>
2024-06-15 23:54:46 +0400

    update LoginBtn.tsx

--- src/features/loginPage/LoginBtn.tsx
+++ src/features/loginPage/LoginBtn.tsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 const LoginBtn = () => {
   return (
     <div>
       <button className="bg-azure-blue hover:bg-deep-blue transition-all ease-in duration-200 w-full text-white py-2 rounded-lg mb-3">
         Sign in
       </button>
-      <div className="flex items-center ">
-        <span className="border w-1/2"> </span>
-        <p className="w-full text-center text-azure-blue text-[14px] cursor-pointer hover:underline">
+      <div className="flex items-center">
+        <span className="border w-1/2"></span>
+        <Link to='' className="block w-full text-center text-azure-blue text-[14px] cursor-pointer hover:underline">
           Sign in as demo user
-        </p>
-        <span className="border w-1/2"> </span>
+        </Link>
+        <span className="border w-1/2"></span>
       </div>
     </div>
   );
