5f759aaaa2ac3560be47865c26433adf0ed83629
Sandro <vipsandro11@gmail.com>
2024-06-15 23:55:08 +0400

    update RegisterPage.tsx

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,9 +1,18 @@
-
+import RegFormInputs from "../features/registerPage/RegFormInputs";
+import RegFormLinks from "../features/registerPage/RegFormLinks";
+import SignUpBtn from "../features/registerPage/SignUpBtn";
+import FormLayout from "../ui/FormLayour";
+import Logo from "../ui/Logo";
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <FormLayout>
+      <Logo />
+      <RegFormLinks />
+      <RegFormInputs />
+      <SignUpBtn />
+    </FormLayout>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
