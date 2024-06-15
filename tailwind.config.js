9da729d09c7a8f713a82a18d4ee92fa5630b7963
Sandro <vipsandro11@gmail.com>
2024-06-15 19:25:53 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "pale-blue": "#E1EAF9",
+        "sky-blue": "#B5E8F6",
+        "deep-blue": "#1F299C",
+      },
+    },
+  },
+  plugins: [],
+};
