00b83cb8cf621ae1d57955dae343bfd400bece6e
Sandro <vipsandro11@gmail.com>
2024-06-15 23:54:48 +0400

    update LoginFormInputs.tsx

--- src/features/loginPage/LoginFormInputs.tsx
+++ src/features/loginPage/LoginFormInputs.tsx
@@ -4,10 +4,20 @@ const LoginFormInputs = () => {
   return (
     <div className="mb-5">
       <div className="mb-3">
-        <Input id="email" name="email" type="email" />
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Email Address"
+        />
       </div>
       <div>
-        <Input id="password" name="password" type="password" />
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
       </div>
     </div>
   );
