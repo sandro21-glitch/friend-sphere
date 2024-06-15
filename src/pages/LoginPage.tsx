2ce96d0cd541a080bbcee49ca156761d9619d8c5
Sandro <vipsandro11@gmail.com>
2024-06-15 21:09:58 +0400

    update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,24 @@
-
+import LoginBtn from "../features/loginPage/LoginBtn";
+import LoginFormInputs from "../features/loginPage/LoginFormInputs";
+import LoginFormLinks from "../features/loginPage/LoginFormLinks";
+import LoginFormLogo from "../features/loginPage/LoginFormLogo";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <section className="bg-white min-h-screen flex justify-center items-center">
+      <div className="flex items-center justify-center section-center w-full">
+        <div className="w-full max-w-[28rem] ">
+          <form className="w-full">
+            <LoginFormLogo />
+            <LoginFormLinks />
+            <LoginFormInputs />
+            <LoginBtn />
+          </form>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
