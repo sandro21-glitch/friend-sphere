78da475e89d850eed9eb271017f0ee247ab59c52
Sandro <vipsandro11@gmail.com>
2024-06-15 21:09:36 +0400

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "pale-blue": "#E1EAF9",
         "sky-blue": "#B5E8F6",
         "deep-blue": "#1F299C",
+        "azure-blue": "#007FFF",
       },
     },
   },
