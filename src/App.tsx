0f66ef9a9f3dfa52762e320096dcc796faad5e23
Sandro <vipsandro11@gmail.com>
2024-06-15 20:09:34 +0400

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <LoginPage />,
+      },
+      {
+        path: "/signin",
+        element: <RegisterPage />,
+      },
+      {
+        path: "dashboard",
+        element: "",
+        children: [],
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
