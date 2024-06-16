63fd6f5e28673c2ad0c9b6437f2b4bb1cf06e8f9
Sandro <vipsandro11@gmail.com>
2024-06-16 14:45:26 +0400

    update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../ui/Navbar";
+import LeftPanel from "../features/Dashboard/LeftPanel";
 
 const Dashboard = () => {
   return (
     <main>
       <Navbar />
-      <Outlet />
+      <section
+        className="section-center section-x"
+        style={{ marginTop: "1rem" }}
+      >
+        <LeftPanel />
+        <Outlet />
+      </section>
     </main>
   );
 };
