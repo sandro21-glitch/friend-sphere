22104d42465e73bee294cb3d8fe481bbcb0de20f
Sandro <vipsandro11@gmail.com>
2024-06-16 14:45:22 +0400

    update LoginFormLinks.tsx

--- src/features/loginPage/LoginFormLinks.tsx
+++ src/features/loginPage/LoginFormLinks.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const LoginFormLinks = () => {
   return (
-    <div className="flex items-center justify-center mb-5">
+    <div className="flex items-center justify-center my-5">
       <Link
         to="/signin"
         className="block w-1/3 text-center pb-2 border-b-2 border-azure-blue"
