c7bf44461c9722d146aaebfe5cd56f2f98ae597b
Sandro <vipsandro11@gmail.com>
2024-06-17 20:30:51 +0400

    feat: create PanelNavigation.tsx

---
+++ src/features/Dashboard/leftPanel/PanelNavigation.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { panelItems } from "../../../constants/panelNavItems";
+
+const PanelNavigation = () => {
+  return (
+    <ul className="flex flex-col gap-5">
+      {panelItems.map((item, index) => {
+        return (
+          <li
+            key={index}
+            className="hover:text-azure-blue transition-colors ease-in duration-150"
+          >
+            <Link
+              to={`/dashboard/${item.path}`}
+              className="flex items-center gap-2 text-[18px] "
+            >
+              {item.icon} {item.name}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default PanelNavigation;
