f12a6300fc5f3c5da627dbf9601e40ed895ede49
Sandro <vipsandro11@gmail.com>
2024-06-17 22:18:20 +0400

    delete 1 file and create 1 file

---
+++ src/features/Dashboard/rightPanel/PostFeed.tsx
@@ -0,0 +1,11 @@
+import FollowingUserPosts from "./postFeed/FollowingUserPosts";
+
+const PostFeed = () => {
+  return (
+    <div className="col-span-2 min-h-full h-full mt-5">
+      <FollowingUserPosts />
+    </div>
+  );
+};
+
+export default PostFeed;
