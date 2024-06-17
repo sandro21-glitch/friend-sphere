697bb9f78c272dab4ddb6106e9db1db27ce93513
Sandro <vipsandro11@gmail.com>
2024-06-17 22:41:02 +0400

    feat: create RightPanel.tsx

---
+++ src/features/Dashboard/RightPanel.tsx
@@ -0,0 +1,11 @@
+import Connections from "./rightPanel/Connections";
+
+const RightPanel = () => {
+  return (
+    <div className="bg-white col-span-1 h-[85vh] sticky top-[5rem] border rounded-md">
+      <Connections />
+    </div>
+  );
+};
+
+export default RightPanel;
