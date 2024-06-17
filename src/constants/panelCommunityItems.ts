0e8b6ee516240369657828bb84211c831b402000
Sandro <vipsandro11@gmail.com>
2024-06-17 20:30:32 +0400

    feat: create panelCommunityItems.ts

---
+++ src/constants/panelCommunityItems.ts
@@ -0,0 +1,22 @@
+export const communityItems = [
+  {
+    name: "Travel",
+    path: "/",
+  },
+  {
+    name: "Programming",
+    path: "/",
+  },
+  {
+    name: "Food and Cooking",
+    path: "/",
+  },
+  {
+    name: "Sports",
+    path: "/",
+  },
+  {
+    name: "Education",
+    path: "/",
+  },
+];
