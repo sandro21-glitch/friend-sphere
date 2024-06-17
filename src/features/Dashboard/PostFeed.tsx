5306f03f19e09d2c632b52955eea2d53b20eedbf
Sandro <vipsandro11@gmail.com>
2024-06-17 20:30:42 +0400

    feat: create PostFeed.tsx

---
+++ src/features/Dashboard/PostFeed.tsx
@@ -0,0 +1,148 @@
+const PostFeed = () => {
+  return (
+    <div className="bg-green-500 col-span-2 min-h-full h-full mt-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto
+      repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tenetur iusto repellat ad sit mollitia optio, neque
+      vitae minus esse cum et nihil impedit? Nisi dicta blanditiis accusantium
+      quidem deserunt cum voluptatem laboriosam dolores atque expedita? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto repellat
+      ad sit mollitia optio, neque vitae minus esse cum et nihil impedit? Nisi
+      dicta blanditiis accusantium quidem deserunt cum voluptatem laboriosam
+      dolores atque expedita? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur iusto repellat ad sit mollitia optio, neque vitae minus esse
+      cum et nihil impedit? Nisi dicta blanditiis accusantium quidem deserunt
+      cum voluptatem laboriosam dolores atque expedita? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Tenetur iusto repellat ad sit mollitia
+      optio, neque vitae minus esse cum et nihil impedit? Nisi dicta blanditiis
+      accusantium quidem deserunt cum voluptatem laboriosam dolores atque
+      expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+      iusto repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tenetur iusto repellat ad sit mollitia optio, neque
+      vitae minus esse cum et nihil impedit? Nisi dicta blanditiis accusantium
+      quidem deserunt cum voluptatem laboriosam dolores atque expedita? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto repellat
+      ad sit mollitia optio, neque vitae minus esse cum et nihil impedit? Nisi
+      dicta blanditiis accusantium quidem deserunt cum voluptatem laboriosam
+      dolores atque expedita? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur iusto repellat ad sit mollitia optio, neque vitae minus esse
+      cum et nihil impedit? Nisi dicta blanditiis accusantium quidem deserunt
+      cum voluptatem laboriosam dolores atque expedita? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Tenetur iusto repellat ad sit mollitia
+      optio, neque vitae minus esse cum et nihil impedit? Nisi dicta blanditiis
+      accusantium quidem deserunt cum voluptatem laboriosam dolores atque
+      expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+      iusto repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tenetur iusto repellat ad sit mollitia optio, neque
+      vitae minus esse cum et nihil impedit? Nisi dicta blanditiis accusantium
+      quidem deserunt cum voluptatem laboriosam dolores atque expedita? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto repellat
+      ad sit mollitia optio, neque vitae minus esse cum et nihil impedit? Nisi
+      dicta blanditiis accusantium quidem deserunt cum voluptatem laboriosam
+      dolores atque expedita? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur iusto repellat ad sit mollitia optio, neque vitae minus esse
+      cum et nihil impedit? Nisi dicta blanditiis accusantium quidem deserunt
+      cum voluptatem laboriosam dolores atque expedita? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Tenetur iusto repellat ad sit mollitia
+      optio, neque vitae minus esse cum et nihil impedit? Nisi dicta blanditiis
+      accusantium quidem deserunt cum voluptatem laboriosam dolores atque
+      expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+      iusto repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tenetur iusto repellat ad sit mollitia optio, neque
+      vitae minus esse cum et nihil impedit? Nisi dicta blanditiis accusantium
+      quidem deserunt cum voluptatem laboriosam dolores atque expedita? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto repellat
+      ad sit mollitia optio, neque vitae minus esse cum et nihil impedit? Nisi
+      dicta blanditiis accusantium quidem deserunt cum voluptatem laboriosam
+      dolores atque expedita? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur iusto repellat ad sit mollitia optio, neque vitae minus esse
+      cum et nihil impedit? Nisi dicta blanditiis accusantium quidem deserunt
+      cum voluptatem laboriosam dolores atque expedita? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Tenetur iusto repellat ad sit mollitia
+      optio, neque vitae minus esse cum et nihil impedit? Nisi dicta blanditiis
+      accusantium quidem deserunt cum voluptatem laboriosam dolores atque
+      expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+      iusto repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tenetur iusto repellat ad sit mollitia optio, neque
+      vitae minus esse cum et nihil impedit? Nisi dicta blanditiis accusantium
+      quidem deserunt cum voluptatem laboriosam dolores atque expedita? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto repellat
+      ad sit mollitia optio, neque vitae minus esse cum et nihil impedit? Nisi
+      dicta blanditiis accusantium quidem deserunt cum voluptatem laboriosam
+      dolores atque expedita? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur iusto repellat ad sit mollitia optio, neque vitae minus esse
+      cum et nihil impedit? Nisi dicta blanditiis accusantium quidem deserunt
+      cum voluptatem laboriosam dolores atque expedita? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Tenetur iusto repellat ad sit mollitia
+      optio, neque vitae minus esse cum et nihil impedit? Nisi dicta blanditiis
+      accusantium quidem deserunt cum voluptatem laboriosam dolores atque
+      expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+      iusto repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tenetur iusto repellat ad sit mollitia optio, neque
+      vitae minus esse cum et nihil impedit? Nisi dicta blanditiis accusantium
+      quidem deserunt cum voluptatem laboriosam dolores atque expedita? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto repellat
+      ad sit mollitia optio, neque vitae minus esse cum et nihil impedit? Nisi
+      dicta blanditiis accusantium quidem deserunt cum voluptatem laboriosam
+      dolores atque expedita? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur iusto repellat ad sit mollitia optio, neque vitae minus esse
+      cum et nihil impedit? Nisi dicta blanditiis accusantium quidem deserunt
+      cum voluptatem laboriosam dolores atque expedita? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Tenetur iusto repellat ad sit mollitia
+      optio, neque vitae minus esse cum et nihil impedit? Nisi dicta blanditiis
+      accusantium quidem deserunt cum voluptatem laboriosam dolores atque
+      expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+      iusto repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tenetur iusto repellat ad sit mollitia optio, neque
+      vitae minus esse cum et nihil impedit? Nisi dicta blanditiis accusantium
+      quidem deserunt cum voluptatem laboriosam dolores atque expedita? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto repellat
+      ad sit mollitia optio, neque vitae minus esse cum et nihil impedit? Nisi
+      dicta blanditiis accusantium quidem deserunt cum voluptatem laboriosam
+      dolores atque expedita? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur iusto repellat ad sit mollitia optio, neque vitae minus esse
+      cum et nihil impedit? Nisi dicta blanditiis accusantium quidem deserunt
+      cum voluptatem laboriosam dolores atque expedita? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Tenetur iusto repellat ad sit mollitia
+      optio, neque vitae minus esse cum et nihil impedit? Nisi dicta blanditiis
+      accusantium quidem deserunt cum voluptatem laboriosam dolores atque
+      expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+      iusto repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Tenetur iusto repellat ad sit mollitia optio, neque
+      vitae minus esse cum et nihil impedit? Nisi dicta blanditiis accusantium
+      quidem deserunt cum voluptatem laboriosam dolores atque expedita? Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto repellat
+      ad sit mollitia optio, neque vitae minus esse cum et nihil impedit? Nisi
+      dicta blanditiis accusantium quidem deserunt cum voluptatem laboriosam
+      dolores atque expedita? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur iusto repellat ad sit mollitia optio, neque vitae minus esse
+      cum et nihil impedit? Nisi dicta blanditiis accusantium quidem deserunt
+      cum voluptatem laboriosam dolores atque expedita? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Tenetur iusto repellat ad sit mollitia
+      optio, neque vitae minus esse cum et nihil impedit? Nisi dicta blanditiis
+      accusantium quidem deserunt cum voluptatem laboriosam dolores atque
+      expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+      iusto repellat ad sit mollitia optio, neque vitae minus esse cum et nihil
+      impedit? Nisi dicta blanditiis accusantium quidem deserunt cum voluptatem
+      laboriosam dolores atque expedita?
+    </div>
+  );
+};
+
+export default PostFeed;
