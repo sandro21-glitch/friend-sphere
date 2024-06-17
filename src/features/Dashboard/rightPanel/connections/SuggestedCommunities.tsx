c1da4feeda1954e53bbae51f59a1ae773c021992
Sandro <vipsandro11@gmail.com>
2024-06-17 22:41:13 +0400

    feat: create SuggestedCommunities.tsx

---
+++ src/features/Dashboard/rightPanel/connections/SuggestedCommunities.tsx
@@ -0,0 +1,12 @@
+const SuggestedCommunities = () => {
+  return (
+    <div className="leading-10">
+      <h5 className="font-semibold text-[16px]">Suggested Communities</h5>
+      <p className="text-gray-500 text-[14px]">
+        No communities to join. Check back later
+      </p>
+    </div>
+  );
+};
+
+export default SuggestedCommunities;
