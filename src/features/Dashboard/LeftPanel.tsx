991b92b188191f3929511e63c831765a8f390337
Sandro <vipsandro11@gmail.com>
2024-06-17 20:30:39 +0400

    update LeftPanel.tsx

--- src/features/Dashboard/LeftPanel.tsx
+++ src/features/Dashboard/LeftPanel.tsx
@@ -1,9 +1,16 @@
-
+import PanelCommunities from "./leftPanel/PanelCommunities";
+import PanelNavigation from "./leftPanel/PanelNavigation";
 
 const LeftPanel = () => {
   return (
-    <div>LeftPanel</div>
-  )
-}
+    <div className="col-span-1 h-[85vh] sticky top-[5rem] border rounded-md py-5 bg-white">
+      <div className="p-5">
+        <PanelNavigation />
+        <hr />
+        <PanelCommunities />
+      </div>
+    </div>
+  );
+};
 
-export default LeftPanel
+export default LeftPanel;
