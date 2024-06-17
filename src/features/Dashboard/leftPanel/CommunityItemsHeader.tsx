e9d057a6cdffb83416c87dc0f49466e317b03af6
Sandro <vipsandro11@gmail.com>
2024-06-17 22:18:23 +0400

    update CommunityItemsHeader.tsx

--- src/features/Dashboard/leftPanel/CommunityItemsHeader.tsx
+++ src/features/Dashboard/leftPanel/CommunityItemsHeader.tsx
@@ -2,24 +2,24 @@ import { HiOutlineUserGroup } from "react-icons/hi";
 import { Link } from "react-router-dom";
 const CommunityItemsHeader = () => {
   return (
-    <div className="flex items-center justify-between">
-    <div className="flex items-center gap-2 mb-5">
-      <HiOutlineUserGroup className="text-[20px]" />
-      Communities
-    </div>
-    <div className="relative">
-      <Link to="/" className="text-azure-blue">
-        See all
-      </Link>
-      <span
-        className="absolute w-4 h-4 bg-azure-blue text-white text-[10px]
+    <div className="flex items-center justify-between mb-5">
+      <div className="flex items-center gap-2">
+        <HiOutlineUserGroup className="text-[20px]" />
+        Communities
+      </div>
+      <div className="relative">
+        <Link to="/" className="text-azure-blue">
+          See all
+        </Link>
+        <span
+          className="absolute w-4 h-4 bg-azure-blue text-white text-[10px]
        flex items-center justify-center rounded-full -right-3 -top-2"
-      >
-        10
-      </span>
+        >
+          10
+        </span>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default CommunityItemsHeader
+export default CommunityItemsHeader;
