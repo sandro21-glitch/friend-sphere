4f35c5aec68b78f72e76be857407c81e33cd7cf2
Sandro <vipsandro11@gmail.com>
2024-06-18 20:40:59 +0400

    update RegFormInputs.tsx

--- src/features/registerPage/RegFormInputs.tsx
+++ src/features/registerPage/RegFormInputs.tsx
@@ -1,16 +1,46 @@
 import Input from "../../ui/Input";
 
-const RegFormInputs = () => {
+type RegFormTypes = {
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  registerUser: {
+    name: string;
+    email: string;
+    password: string;
+  };
+};
+
+const RegFormInputs = ({ handleChange, registerUser }: RegFormTypes) => {
   return (
     <div className="mb-5">
       <div className="mb-3">
-        <Input id="name" name="name" type="text" placeholder="Username" />
+        <Input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Username"
+          onChange={handleChange}
+          value={registerUser.name}
+        />
       </div>
       <div className="mb-3">
-        <Input id="email" name="email" type="email" placeholder="Email address" />
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Email address"
+          onChange={handleChange}
+          value={registerUser.email}
+        />
       </div>
       <div>
-        <Input id="password" name="password" type="password" placeholder="Password" />
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={registerUser.password}
+        />
       </div>
     </div>
   );
