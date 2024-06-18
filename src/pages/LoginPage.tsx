e8c67deb31c1d73ff6d9964a721c02ac74fd2f27
Sandro <vipsandro11@gmail.com>
2024-06-18 23:10:27 +0400

    update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,15 +1,49 @@
+import { useState } from "react";
 import LoginBtn from "../features/loginPage/LoginBtn";
 import LoginFormInputs from "../features/loginPage/LoginFormInputs";
 import LoginFormLinks from "../features/loginPage/LoginFormLinks";
 import FormLayout from "../ui/FormLayour";
 import Logo from "../ui/Logo";
+import { loginUser } from "../slices/user/userThunks";
+import { useAppDispatch } from "../hooks/reduxHooks";
 
+type LoginFormType = {
+  email: string;
+  password: string;
+};
 const LoginPage = () => {
+  const dispatch = useAppDispatch();
+  const [userLoginForm, setUserLoginForm] = useState<LoginFormType>({
+    email: "",
+    password: "",
+  });
+
+  const handleLoginUser = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      await dispatch(loginUser(userLoginForm));
+      console.log("login successful!");
+    } catch (error) {
+      console.error("login error:", error);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUserLoginForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
-    <FormLayout>
+    <FormLayout onSubmit={handleLoginUser}>
       <Logo height="4rem" />
       <LoginFormLinks />
-      <LoginFormInputs />
+      <LoginFormInputs
+        handleChange={handleChange}
+        userLoginForm={userLoginForm}
+      />
       <LoginBtn />
     </FormLayout>
   );
