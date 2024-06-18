32c252f6ff65a89ec4ab60fbe96af573ccf9cb4b
Sandro <vipsandro11@gmail.com>
2024-06-18 20:41:01 +0400

    update SignUpBtn.tsx

--- src/features/registerPage/SignUpBtn.tsx
+++ src/features/registerPage/SignUpBtn.tsx
@@ -1,6 +1,9 @@
 const SignUpBtn = () => {
   return (
-    <button className="bg-azure-blue hover:bg-deep-blue transition-all ease-in duration-200 w-full text-white py-2 rounded-lg mb-3">
+    <button
+      type="submit"
+      className="bg-azure-blue hover:bg-deep-blue transition-all ease-in duration-200 w-full text-white py-2 rounded-lg mb-3"
+    >
       Sign Up
     </button>
   );
