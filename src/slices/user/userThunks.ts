d2764a1aed29631f2886396c928a51439fc63518
Sandro <vipsandro11@gmail.com>
2024-06-18 22:53:35 +0400

    update userThunks.ts

--- src/slices/user/userThunks.ts
+++ src/slices/user/userThunks.ts
@@ -1,7 +1,10 @@
-import { ref, set } from "firebase/database";
+import { get, ref, set } from "firebase/database";
 import { auth, database } from "../../config/firebase";
 import { nanoid } from "nanoid";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { UserData, userFormData } from "./userTypes";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
@@ -41,6 +44,48 @@ export const registerUser = createAsyncThunk(
       return userProfile;
     } catch (error: any) {
       return rejectWithValue(error.message || "Registration failed");
+    }
+  }
+);
+
+// Login user thunk
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (userData: UserData, { rejectWithValue }) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        userData.email,
+        userData.password
+      );
+      const { uid, email } = userCredential.user;
+      if (!email) {
+        throw new Error("Email cannot be null");
+      }
+
+      // Fetch the user's profile data
+      const userRef = ref(database, `users/${uid}`);
+      const userSnapshot = await get(userRef);
+
+      if (!userSnapshot.exists()) {
+        throw new Error("User profile does not exist");
+      }
+
+      const userProfile = userSnapshot.val();
+      // const {
+      //   email,
+      //   password,
+      //   name,
+      //   followers,
+      //   following,
+      //   registeredDate,
+      //   joinedGroups,
+      //   isAdmin,
+      // } = userProfile;
+
+      return { userProfile };
+    } catch (error: any) {
+      return rejectWithValue(error.message || "Login failed");
     }
   }
 );
