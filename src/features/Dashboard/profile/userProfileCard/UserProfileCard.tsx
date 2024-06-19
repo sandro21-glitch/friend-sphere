a807646984137e38773f16756456d42fb91a22c9
Sandro <vipsandro11@gmail.com>
2024-06-19 18:23:20 +0400

    feat: create UserProfileCard.tsx

---
+++ src/features/Dashboard/profile/userProfileCard/UserProfileCard.tsx
@@ -0,0 +1,13 @@
+import EditProfileBtn from "./EditProfileBtn";
+import ProfileHeader from "./ProfileHeader";
+
+const UserProfileCard = () => {
+  return (
+    <div className="w-full">
+      <EditProfileBtn />
+      <ProfileHeader />
+    </div>
+  );
+};
+
+export default UserProfileCard;
