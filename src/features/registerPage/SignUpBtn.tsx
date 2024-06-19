c0db5cb70f924ebcd73a40eb937eccb80303b13a
Sandro <vipsandro11@gmail.com>
2024-06-19 21:26:22 +0400

    update SignUpBtn.tsx

--- src/features/registerPage/SignUpBtn.tsx
+++ src/features/registerPage/SignUpBtn.tsx
@@ -1,10 +1,21 @@
+import { useAppSelector } from "../../hooks/reduxHooks";
+import SmallSpinner from "../../ui/SmallSpinner";
+
 const SignUpBtn = () => {
+  const { loading } = useAppSelector((store) => store.auth);
   return (
     <button
       type="submit"
       className="bg-azure-blue hover:bg-deep-blue transition-all ease-in duration-200 w-full text-white py-2 rounded-lg mb-3"
     >
-      Sign Up
+      {loading ? (
+        <div className="text-[14px]">
+          Registering...
+          <SmallSpinner />
+        </div>
+      ) : (
+        "Sign Up"
+      )}
     </button>
   );
 };
