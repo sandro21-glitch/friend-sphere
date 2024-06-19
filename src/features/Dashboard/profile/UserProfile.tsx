b1592b89b227aa1021633ef240551217178f5cf5
Sandro <vipsandro11@gmail.com>
2024-06-19 17:38:53 +0400

    feat: create UserProfile.tsx

---
+++ src/features/Dashboard/profile/UserProfile.tsx
@@ -0,0 +1,11 @@
+import EditProfileBtn from "./EditProfileBtn";
+
+const UserProfile = () => {
+  return (
+    <div className="col-span-2 min-h-full h-full mt-5 bg-white border rounded-md p-5">
+      <EditProfileBtn />
+    </div>
+  );
+};
+
+export default UserProfile;
