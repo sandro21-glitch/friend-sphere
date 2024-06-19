da83f314a35a3399249c4f87b1cccc394a139a36
Sandro <vipsandro11@gmail.com>
2024-06-19 17:38:55 +0400

    feat: create EditProfileBtn.tsx

---
+++ src/features/Dashboard/profile/EditProfileBtn.tsx
@@ -0,0 +1,11 @@
+import { CiEdit } from "react-icons/ci";
+
+const EditProfileBtn = () => {
+  return (
+    <div className="relative w-full flex items-end">
+      <CiEdit className="text-[25px]" />
+    </div>
+  );
+};
+
+export default EditProfileBtn;
