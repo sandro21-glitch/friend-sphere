9d10cec4dbb8e459ba309fc8b671f9ac530e8aa7
Sandro <vipsandro11@gmail.com>
2024-06-19 21:26:18 +0400

    update UserProfileCard.tsx

--- src/features/Dashboard/profile/userProfileCard/UserProfileCard.tsx
+++ src/features/Dashboard/profile/userProfileCard/UserProfileCard.tsx
@@ -1,11 +1,15 @@
 import EditProfileBtn from "./EditProfileBtn";
 import ProfileHeader from "./ProfileHeader";
+import UserInterests from "./UserInterests";
+import UserLocation from "./UserLocation";
 
 const UserProfileCard = () => {
   return (
     <div className="w-full">
       <EditProfileBtn />
       <ProfileHeader />
+      <UserLocation />
+      <UserInterests />
     </div>
   );
 };
