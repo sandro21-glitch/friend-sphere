e412c4a859d53db9528892283ee032b67e8b2d24
Sandro <vipsandro11@gmail.com>
2024-06-19 21:26:27 +0400

    feat: create SmallSpinner.tsx

---
+++ src/ui/SmallSpinner.tsx
@@ -0,0 +1,14 @@
+const SmallSpinner = () => {
+  return (
+    <div
+      className="animate-spin inline-block size-3 border-[3px] border-current
+       border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
+      role="status"
+      aria-label="loading"
+    >
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+};
+
+export default SmallSpinner;
