708edfeb512ae94f1b378d88c138a8de5414fe24
Sandro <vipsandro11@gmail.com>
2024-06-20 22:46:23 +0400

    update modalSlice.ts

--- src/slices/modals/modalSlice.ts
+++ src/slices/modals/modalSlice.ts
@@ -3,13 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 // import type { RootState } from "../store";
 
-
 interface ModalState {
   userProfileModal: boolean;
+  updateProfileModal: boolean;
 }
 
 const initialState: ModalState = {
   userProfileModal: false,
+  updateProfileModal: false,
 };
 
 export const modalSlice = createSlice({
@@ -19,9 +20,13 @@ export const modalSlice = createSlice({
     setUserProfileModal: (state, action: PayloadAction<boolean>) => {
       state.userProfileModal = action.payload;
     },
+    setUpdateProfileModal: (state, action: PayloadAction<boolean>) => {
+      state.updateProfileModal = action.payload;
+    },
   },
 });
 
-export const { setUserProfileModal } = modalSlice.actions;
+export const { setUserProfileModal, setUpdateProfileModal } =
+  modalSlice.actions;
 
 export default modalSlice.reducer;
