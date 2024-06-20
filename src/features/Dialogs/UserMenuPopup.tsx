8f352ff74026411651145e105ef8f6ef8954b319
Sandro <vipsandro11@gmail.com>
2024-06-20 18:41:47 +0400

    feat: create UserMenuPopup.tsx

---
+++ src/features/Dialogs/UserMenuPopup.tsx
@@ -0,0 +1,27 @@
+import { IoIosLogOut } from "react-icons/io";
+
+const UserMenuPopup = () => {
+  return (
+    <div className="absolute right-5 top-full pt-3 w-[18rem] z-[99999]">
+      <div className="border p-2 bg-white rounded-md flex flex-col items-center">
+        <img
+          src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
+          alt="user image"
+          className="w-14 h-14 mb-2"
+        />
+        <h2 className="text-[16px] font-semibold">Username</h2>
+        <p className="text-gray-500 text-[14px]">demouser@socialecho.com</p>
+        <hr />
+        <button
+          className="text-red-500 hover:text-red-700 transition-colors ease-in duration-200 mb-2 text-[14px]
+        flex items-center gap-1"
+        >
+          Logout
+          <IoIosLogOut />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenuPopup;
