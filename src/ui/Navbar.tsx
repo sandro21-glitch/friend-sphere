270972e419807e090c38b091303d446aebb67863
Sandro <vipsandro11@gmail.com>
2024-06-20 19:08:34 +0400

    update Navbar.tsx

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,8 +1,16 @@
 import UserMenuPopup from "../features/Dialogs/UserMenuPopup";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
+import { setUserProfileModal } from "../slices/modals/modalSlice";
 import Input from "./Input";
 import Logo from "./Logo";
 import { FaUserCircle } from "react-icons/fa";
 const Navbar = () => {
+  const { userProfileModal } = useAppSelector((store) => store.modals);
+  const dispatch = useAppDispatch();
+  const handleOpenModal = () => {
+    dispatch(setUserProfileModal(!userProfileModal));
+  };
+
   return (
     <header className="p-[.5rem] border-b bg-white sticky inset-0 z-[999999]">
       <nav className=" section-center section-x flex items-center justify-between gap-10 relative">
@@ -14,8 +22,11 @@ const Navbar = () => {
           placeholder="Search for people, posts or communities"
           className="max-w-[660px] py-2 rounded-full text-[14px]"
         />
-        <FaUserCircle className="min-w-7 min-h-7 cursor-pointer text-gray-400" />
-        <UserMenuPopup />
+        <FaUserCircle
+          className="min-w-7 min-h-7 cursor-pointer text-gray-400"
+          onClick={handleOpenModal}
+        />
+        {userProfileModal && <UserMenuPopup />}
       </nav>
     </header>
   );
