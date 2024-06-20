bee8980b519a870022f27f497659308123e6f483
Sandro <vipsandro11@gmail.com>
2024-06-20 22:46:51 +0400

    feat: create EditProfilePopup.tsx

---
+++ src/features/Dialogs/EditProfilePopup.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Bio from "./editProfilePopup/Bio";
+import Interests from "./editProfilePopup/Interests";
+import ListOfInterests from "./editProfilePopup/ListOfInterests";
+import Location from "./editProfilePopup/Location";
+import EditProfileActions from "./editProfilePopup/EditProfileActions";
+
+const EditProfilePopup = () => {
+  const [interestList, setInterestList] = useState<string[]>([]);
+
+  return (
+    <div className="w-full max-w-[36rem] mx-auto bg-white p-6 rounded shadow-lg">
+      <h2 className="text-[18px] font-medium mb-5">Update Profile</h2>
+      <Bio />
+      <Location />
+      <Interests
+        interestList={interestList}
+        setInterestList={setInterestList}
+      />
+      <ListOfInterests
+        setInterestList={setInterestList}
+        interestList={interestList}
+      />
+      <EditProfileActions />
+    </div>
+  );
+};
+
+export default EditProfilePopup;
