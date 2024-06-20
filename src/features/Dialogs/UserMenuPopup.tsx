1a96763b85a9b49d646740e4595a7e52a5f08bca
Sandro <vipsandro11@gmail.com>
2024-06-20 19:08:30 +0400

    update UserMenuPopup.tsx

--- src/features/Dialogs/UserMenuPopup.tsx
+++ src/features/Dialogs/UserMenuPopup.tsx
@@ -2,14 +2,14 @@ import { IoIosLogOut } from "react-icons/io";
 
 const UserMenuPopup = () => {
   return (
-    <div className="absolute right-5 top-full pt-3 w-[18rem] z-[99999]">
+    <div className="absolute right-5 top-full pt-3 w-[18rem] z-[99999]  select-none">
       <div className="border p-2 bg-white rounded-md flex flex-col items-center">
         <img
           src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
           alt="user image"
-          className="w-14 h-14 mb-2"
+          className="w-14 h-14 mb-2 cursor-pointer"
         />
-        <h2 className="text-[16px] font-semibold">Username</h2>
+        <h2 className="text-[16px] font-semibold cursor-pointer">Username</h2>
         <p className="text-gray-500 text-[14px]">demouser@socialecho.com</p>
         <hr />
         <button
