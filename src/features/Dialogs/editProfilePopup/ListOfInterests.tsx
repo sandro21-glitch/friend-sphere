ffe8b1f0bb5873a90edfa0a2d255d01898aed451
Sandro <vipsandro11@gmail.com>
2024-06-21 20:35:57 +0400

    update ListOfInterests.tsx

--- src/features/Dialogs/editProfilePopup/ListOfInterests.tsx
+++ src/features/Dialogs/editProfilePopup/ListOfInterests.tsx
@@ -1,14 +1,12 @@
+import React from "react";
 import { interests } from "../../../constants/interests";
 
-type ListOfInterestTypes = {
+type ListOfInterestsProps = {
   setInterestList: React.Dispatch<React.SetStateAction<string[]>>;
   interestList: string[];
 };
 
-const ListOfInterests = ({
-  setInterestList,
-  interestList,
-}: ListOfInterestTypes) => {
+const ListOfInterests: React.FC<ListOfInterestsProps> = ({ setInterestList, interestList }) => {
   const handlePushInterest = (interest: string) => {
     if (interestList.length < 5 && !interestList.includes(interest)) {
       setInterestList((prev) => [...prev, interest]);
@@ -17,18 +15,15 @@ const ListOfInterests = ({
 
   return (
     <ul className="flex flex-wrap gap-3 mb-3">
-      {interests.map((interest, index) => {
-        return (
-          <li
-            key={index}
-            onClick={() => handlePushInterest(interest)}
-            className="text-[11px] bg-gray-200 hover:bg-gray-300
-             cursor-pointer px-2 py-1 rounded-full transition-colors ease-in duration-150"
-          >
-            {interest}
-          </li>
-        );
-      })}
+      {interests.map((interest, index) => (
+        <li
+          key={index}
+          onClick={() => handlePushInterest(interest)}
+          className="text-[11px] bg-gray-200 hover:bg-gray-300 cursor-pointer px-2 py-1 rounded-full transition-colors ease-in duration-150"
+        >
+          {interest}
+        </li>
+      ))}
     </ul>
   );
 };
