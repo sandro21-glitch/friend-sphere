60ab3923172d565e879bbfbbda6a1fbacb48ade9
Sandro <vipsandro11@gmail.com>
2024-06-21 22:47:52 +0400

    update Modals.tsx

--- src/features/Dialogs/Modals.tsx
+++ src/features/Dialogs/Modals.tsx
@@ -3,12 +3,13 @@ import EditProfilePopup from "./EditProfilePopup";
 
 const Modals = () => {
   const { updateProfileModal } = useAppSelector((store) => store.modals);
-
+  const isAnyModalOpen = updateProfileModal;
+  if (!isAnyModalOpen) return null;
   return (
     <div className="w-full h-full absolute inset-0 flex items-center justify-center z-[9999] min-h-screen">
       <div className="fixed inset-0 bg-black opacity-30"></div>
       <div className="z-[9999]">
-        {!updateProfileModal ? <EditProfilePopup /> : null}
+        {updateProfileModal ? <EditProfilePopup /> : null}
       </div>
     </div>
   );
