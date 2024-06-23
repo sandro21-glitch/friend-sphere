348c1c6424a4f0b342087d8acec1a5347f05d2de
Sandro <vipsandro11@gmail.com>
2024-06-23 21:45:18 +0400

    feat: Enhance UserLocation component with dynamic user data

--- src/features/Dashboard/profile/userProfileCard/UserLocation.tsx
+++ src/features/Dashboard/profile/userProfileCard/UserLocation.tsx
@@ -1,11 +1,14 @@
 import { CiLocationOn } from "react-icons/ci";
+import { useAppSelector } from "../../../../hooks/reduxHooks";
 const UserLocation = () => {
+  const { userData } = useAppSelector((store) => store.auth);
+
   return (
     <div className="mb-3">
       <p className="text-[16px] font-semibold">Location</p>
       <p className="text-[14px] flex items-center gap-1">
         <CiLocationOn />
-        Somewhere
+        {userData?.location || "Somewhere"}
       </p>
     </div>
   );
