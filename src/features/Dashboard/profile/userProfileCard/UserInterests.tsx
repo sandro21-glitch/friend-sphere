2ad58374ef9fa8afdc5f50611b5591a32a67e1a9
Sandro <vipsandro11@gmail.com>
2024-06-23 21:45:31 +0400

    feat: Update UserInterests component to display user interests dynamically

--- src/features/Dashboard/profile/userProfileCard/UserInterests.tsx
+++ src/features/Dashboard/profile/userProfileCard/UserInterests.tsx
@@ -1,8 +1,24 @@
+import { useAppSelector } from "../../../../hooks/reduxHooks";
+
 const UserInterests = () => {
+  const { userData } = useAppSelector((store) => store.auth);
+
   return (
     <div>
-      <p className="text-[16px] font-semibold">Interests</p>
-      <p className="text-[14px]">✏️ Design</p>
+      <p className="text-[16px] font-semibold mb-1">Interests</p>
+      <ul className="flex items-center gap-2">
+        {userData?.interests.map((interest, index) => {
+          return (
+            <li
+              key={index}
+              className="text-[14px] bg-gray-100 rounded-full py-1 px-3
+               hover:bg-gray-300 transition-colors ease-in duration-150"
+            >
+              {interest}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
