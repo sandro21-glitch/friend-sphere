85706c084adfd3fb2bd7c33efd46ba3af358cf02
Sandro <vipsandro11@gmail.com>
2024-06-23 21:44:51 +0400

    feat: Enhance Bio component with state management

--- src/features/Dialogs/editProfilePopup/Bio.tsx
+++ src/features/Dialogs/editProfilePopup/Bio.tsx
@@ -1,6 +1,12 @@
 import Input from "../../../ui/Input";
 import { FiUser } from "react-icons/fi";
-const Bio = () => {
+
+type BioTypes = {
+  bio: string;
+  setBio: (bio: string) => void;
+};
+
+const Bio = ({ bio, setBio }: BioTypes) => {
   return (
     <div className="mb-3">
       <label htmlFor="bio" className="flex items-center gap-1 text-[14px]">
@@ -11,6 +17,8 @@ const Bio = () => {
         id="bio"
         name="bio"
         type="text"
+        value={bio}
+        onChange={(e) => setBio(e.target.value)}
         className="border-b-2 border-l-0 border-r-0 border-t-0 rounded-full text-[14px]"
       />
     </div>
