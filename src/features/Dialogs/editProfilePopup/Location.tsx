ee71c5056183a36473d730abf26cb6ab6143892f
Sandro <vipsandro11@gmail.com>
2024-06-23 21:44:22 +0400

    feat: Enhance Location component with state management

--- src/features/Dialogs/editProfilePopup/Location.tsx
+++ src/features/Dialogs/editProfilePopup/Location.tsx
@@ -1,6 +1,12 @@
 import Input from "../../../ui/Input";
 import { CiLocationOn } from "react-icons/ci";
-const Location = () => {
+
+type LocationTypes = {
+  location: string;
+  setLocation: (location: string) => void;
+};
+
+const Location = ({ location, setLocation }: LocationTypes) => {
   return (
     <div className="mb-3">
       <label htmlFor="location" className="text-[14px] flex items-center gap-1">
@@ -11,6 +17,8 @@ const Location = () => {
         id="location"
         name="location"
         type="text"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
         className="border-b-2 border-l-0 border-r-0 border-t-0 rounded-full text-[14px]"
       />
     </div>
