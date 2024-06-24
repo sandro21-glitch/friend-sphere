b988cfbdc3059953743e5c990f1f8f91e6328503
Sandro <vipsandro11@gmail.com>
2024-06-24 17:14:49 +0400

    fix: :bug: handle undefined interests array in UserInterests component
    - Added conditional rendering to check for the presence of interests before mapping. - Prevented potential errors when userData.interests is undefined.

--- src/features/Dashboard/profile/userProfileCard/UserInterests.tsx
+++ src/features/Dashboard/profile/userProfileCard/UserInterests.tsx
@@ -6,19 +6,21 @@ const UserInterests = () => {
   return (
     <div>
       <p className="text-[16px] font-semibold mb-1">Interests</p>
-      <ul className="flex items-center gap-2">
-        {userData?.interests.map((interest, index) => {
-          return (
-            <li
-              key={index}
-              className="text-[14px] bg-gray-100 rounded-full py-1 px-3
-               hover:bg-gray-300 transition-colors ease-in duration-150"
-            >
-              {interest}
-            </li>
-          );
-        })}
-      </ul>
+      {userData?.interests ? (
+        <ul className="flex items-center gap-2">
+          {userData?.interests.map((interest, index) => {
+            return (
+              <li
+                key={index}
+                className="text-[14px] bg-gray-100 rounded-full py-1 px-3
+            hover:bg-gray-300 transition-colors ease-in duration-150"
+              >
+                {interest}
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
     </div>
   );
 };
