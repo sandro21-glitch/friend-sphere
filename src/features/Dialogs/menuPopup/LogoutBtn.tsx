f2571cde80d150fc7299757dee2ce2bb5e089c12
Sandro <vipsandro11@gmail.com>
2024-06-24 20:18:12 +0400

    feat: create LogoutBtn.tsx

---
+++ src/features/Dialogs/menuPopup/LogoutBtn.tsx
@@ -0,0 +1,34 @@
+import { IoIosLogOut } from "react-icons/io";
+import { useAppDispatch } from "../../../hooks/reduxHooks";
+import { signOutUser } from "../../../slices/user/authThunks";
+import { useNavigate } from "react-router-dom";
+
+const LogoutBtn = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const logout = await dispatch(signOutUser());
+      if (logout) {
+        navigate("/signin");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleLogout}
+      className="text-red-500 hover:text-red-700 transition-colors ease-in duration-200 mb-2 text-[14px]
+  flex items-center gap-1"
+    >
+      Logout
+      <IoIosLogOut />
+    </button>
+  );
+};
+
+export default LogoutBtn;
