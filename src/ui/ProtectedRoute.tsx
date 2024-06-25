5f987f37e5a954510c88fb9bf4b1ac56186b2a23
Sandro <vipsandro11@gmail.com>
2024-06-25 15:15:06 +0400

    feat: create ProtectedRoute.tsx

---
+++ src/ui/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAppSelector } from "../hooks/reduxHooks";
+
+const ProtectedRoute = () => {
+  const isAuthenticated = useAppSelector((state) => state.auth.userData);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/signin" />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
