f0a21bacfbc58ce58c093a0dc944fdcf95f33f9a
Sandro <vipsandro11@gmail.com>
2024-06-25 20:17:36 +0400

    update userDataSlice.ts

--- src/slices/user/userDataSlice.ts
+++ src/slices/user/userDataSlice.ts
@@ -1,16 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { updateUserProfile } from "./userDataThunks";
+import { UserData } from "./userTypes";
 // import type { PayloadAction } from '@reduxjs/toolkit'
 // import type { RootState } from "../store";
 
 interface UserDataState {
   loading: boolean | null;
   error: string | null;
+  getAllUser: UserData[] | null;
 }
 
 const initialState: UserDataState = {
   loading: null,
   error: null,
+  getAllUser: null,
 };
 
 export const userDataSlice = createSlice({
