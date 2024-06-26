f26606ce9bee5fa493f34294c40a70dae6d38ae9
Sandro <vipsandro11@gmail.com>
2024-06-26 23:08:30 +0400

    update CommunityItemList.tsx

--- src/features/Dashboard/leftPanel/CommunityItemList.tsx
+++ src/features/Dashboard/leftPanel/CommunityItemList.tsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
-import { communityItems } from "../../../constants/panelCommunityItems";
+import { useAppSelector } from "../../../hooks/reduxHooks";
 
 const CommunityItemList = () => {
+  const { communityData, loading, error } = useAppSelector(
+    (store) => store.communities
+  );
+
+  if (error) return <p>ERROR</p>;
+  if (loading) return <p>Loading...</p>;
   return (
     <ul className="flex flex-col gap-1">
-      {communityItems.map((item, index) => {
+      {communityData?.slice(0, 5).map((item, index) => {
         return (
           <li
             key={index}
             className="text-gray-600 hover:text-azure-blue transition-colors ease-in duration-150"
           >
-            <Link to={item.path}>{item.name}</Link>
+            <Link to={item.name}>{item.name}</Link>
           </li>
         );
       })}
