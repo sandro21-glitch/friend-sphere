ee61521584d0d14431d8461ddddb43c6e3b395fc
Sandro <vipsandro11@gmail.com>
2024-06-26 22:50:04 +0400

    feat: create communityThunks.ts

---
+++ src/slices/community/communityThunks.ts
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+import { onValue, ref } from "firebase/database";
+import { database } from "../../config/firebase";
+import { CommunityTypes } from "./communitySlice";
+
+
+export const fetchCommunities = createAsyncThunk(
+  "community/fetchCommunities",
+  async (_, { rejectWithValue }) => {
+    try {
+      const communitiesRef = ref(database, "communities");
+
+      const snapshot = await new Promise<any>((resolve, _) => {
+        onValue(communitiesRef, (snapshot) => {
+          const communities: CommunityTypes[] = [];
+          snapshot.forEach((childSnapshot) => {
+            communities.push(childSnapshot.val() as CommunityTypes);
+          });
+          resolve(communities);
+        }, {
+          onlyOnce: true, // Ensure the listener is called only once
+        });
+      });
+
+      return snapshot;
+    } catch (error: any) {
+      return rejectWithValue(error.message || "Error fetching communities");
+    }
+  }
+);
