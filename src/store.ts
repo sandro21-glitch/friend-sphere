1a4dadf85f24437406c2ce1cedadb6178e918265
Sandro <vipsandro11@gmail.com>
2024-06-26 22:49:43 +0400

    update store.ts

--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/user/authSlice";
 import modalReducer from "./slices/modals/modalSlice";
 import userDataReducer from "./slices/user/userDataSlice";
+import communitiesReducer from "./slices/community/communitySlice";
+
 const store = configureStore({
   reducer: {
     auth: authReducer,
     userData: userDataReducer,
     modals: modalReducer,
+    communities: communitiesReducer,
   },
 });
 
