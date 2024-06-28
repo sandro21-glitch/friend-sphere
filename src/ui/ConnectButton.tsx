5bcab58a2078d4ee6bfa1276d7878b900fb23be0
Sandro <vipsandro11@gmail.com>
2024-06-28 22:28:43 +0400

    feat: create ConnectButton.tsx

---
+++ src/ui/ConnectButton.tsx
@@ -0,0 +1,23 @@
+import { SlUserFollow } from "react-icons/sl";
+import { MdGroups } from "react-icons/md";
+type ConnectButtonTypes = {
+  name: string;
+  follow?: boolean;
+  join?: boolean;
+};
+
+const ConnectButton = ({ name, follow, join }: ConnectButtonTypes) => {
+  return (
+    <button
+      type="button"
+      className="border border-dashed border-azure-blue font-semibold text-azure-blue
+     capitalize px-2 py-1 rounded-md flex items-center gap-2 text-[14px] hover:bg-azure-blue hover:text-white transition-all ease-in duration-150"
+    >
+      {follow && <SlUserFollow className="text-[16px] " />}
+      {join && <MdGroups className="text-[16px]" />}
+      {name}
+    </button>
+  );
+};
+
+export default ConnectButton;
