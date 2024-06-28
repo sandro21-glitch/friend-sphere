e086488eb9d509f1a9e242cb314796682c206b35
Sandro <vipsandro11@gmail.com>
2024-06-28 22:13:37 +0400

    update SuggestedCommunities.tsx

--- src/features/Dashboard/rightPanel/connections/SuggestedCommunities.tsx
+++ src/features/Dashboard/rightPanel/connections/SuggestedCommunities.tsx
@@ -1,11 +1,29 @@
+import { useAppSelector } from "../../../../hooks/reduxHooks";
+
 const SuggestedCommunities = () => {
+  const { nonJoinedGroupData, nonJoinedGroups } = useAppSelector(
+    (store) => store.communities
+  );
+
+  if (nonJoinedGroups.loading) return <p>Loading...</p>;
+  if (nonJoinedGroups.error) return <p>error...</p>;
+
+  if (nonJoinedGroupData && nonJoinedGroupData?.length < 1) {
+    return (
+      <div className="leading-10">
+        <h5 className="font-semibold text-[16px]">Suggested Communities</h5>
+        <p className="text-gray-500 text-[14px]">
+          No communities to join. Check back later
+        </p>
+      </div>
+    );
+  }
   return (
-    <div className="leading-10">
-      <h5 className="font-semibold text-[16px]">Suggested Communities</h5>
-      <p className="text-gray-500 text-[14px]">
-        No communities to join. Check back later
-      </p>
-    </div>
+    <ul>
+      {nonJoinedGroupData?.map((group) => {
+        return <li>{group.name}</li>;
+      })}
+    </ul>
   );
 };
 
