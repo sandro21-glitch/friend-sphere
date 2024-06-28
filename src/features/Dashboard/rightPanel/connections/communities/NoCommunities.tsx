76580abac8975286438b4d2a537b2d6cbadbf8c6
Sandro <vipsandro11@gmail.com>
2024-06-28 22:38:05 +0400

    feat: create NoCommunities.tsx

---
+++ src/features/Dashboard/rightPanel/connections/communities/NoCommunities.tsx
@@ -0,0 +1,14 @@
+
+
+const NoCommunities = () => {
+  return (
+    <div className="leading-10">
+      <h5 className="font-semibold text-[16px]">Suggested Communities</h5>
+      <p className="text-gray-500 text-[14px]">
+        No communities to join. Check back later
+      </p>
+    </div>
+  );
+};
+
+export default NoCommunities;
