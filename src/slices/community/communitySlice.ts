e19cbe848b388af5875ac9b8ac6d0b4489f67ec1
Sandro <vipsandro11@gmail.com>
2024-06-28 22:13:46 +0400

    update communitySlice.ts

--- src/slices/community/communitySlice.ts
+++ src/slices/community/communitySlice.ts
@@ -1,5 +1,8 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { fetchCommunities } from "./communityThunks";
+import {
+  fetchNonJoinedCommunities,
+  fetchUserCommunities,
+} from "./communityThunks";
 
 export interface CommunityTypes {
   name: string;
@@ -31,14 +34,28 @@ export interface CommunityTypes {
 
 interface CommunityState {
   communityData: CommunityTypes[] | null;
-  loading: boolean;
-  error: string | null;
+  nonJoinedGroupData: CommunityTypes[] | null;
+  joinedGroups: {
+    loading: boolean;
+    error: string | null;
+  };
+  nonJoinedGroups: {
+    loading: boolean;
+    error: string | null;
+  };
 }
 
 const initialState: CommunityState = {
   communityData: null,
-  loading: false,
-  error: null,
+  nonJoinedGroupData: null,
+  joinedGroups: {
+    loading: false,
+    error: null,
+  },
+  nonJoinedGroups: {
+    loading: false,
+    error: null,
+  },
 };
 
 export const communitiesSlice = createSlice({
@@ -47,21 +64,35 @@ export const communitiesSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchCommunities.pending, (state) => {
-        state.loading = true;
-        state.error = null;
+      .addCase(fetchUserCommunities.pending, (state) => {
+        state.joinedGroups.loading = true;
+        state.joinedGroups.error = null;
       })
       .addCase(
-        fetchCommunities.fulfilled,
+        fetchUserCommunities.fulfilled,
         (state, action: PayloadAction<CommunityTypes[]>) => {
           state.communityData = action.payload;
-          state.loading = false;
-          state.error = null;
+          state.joinedGroups.loading = false;
+          state.joinedGroups.error = null;
         }
       )
-      .addCase(fetchCommunities.rejected, (state, action) => {
-        state.error = action.error.message ?? "Error fetching communities";
-        state.loading = false;
+      .addCase(fetchUserCommunities.rejected, (state, action) => {
+        state.joinedGroups.error =
+          action.error.message ?? "Error fetching communities";
+        state.joinedGroups.loading = false;
+      });
+    builder
+      .addCase(fetchNonJoinedCommunities.pending, (state) => {
+        state.nonJoinedGroups.loading = true;
+      })
+      .addCase(fetchNonJoinedCommunities.fulfilled, (state, action) => {
+        state.nonJoinedGroups.loading = false;
+        state.nonJoinedGroupData = action.payload;
+      })
+      .addCase(fetchNonJoinedCommunities.rejected, (state, action) => {
+        state.nonJoinedGroups.error =
+          action?.error.message ?? "Error fetching communities";
+        state.nonJoinedGroups.loading = false;
       });
   },
 });
