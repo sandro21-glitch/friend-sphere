d5a9c075afd1c106f6c674c226cbfd87ea0e9894
Sandro <vipsandro11@gmail.com>
2024-06-29 18:23:13 +0400

    update CommunityItemList.tsx

--- src/features/Dashboard/leftPanel/CommunityItemList.tsx
+++ src/features/Dashboard/leftPanel/CommunityItemList.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../../hooks/reduxHooks";
+import SmallSpinner from "../../../ui/SmallSpinner";
 
 const CommunityItemList = () => {
-  const { communityData, loading, error } = useAppSelector(
-    (store) => store.communities
-  );
+  const {
+    communityData,
+    joinedGroups: { error, loading },
+  } = useAppSelector((store) => store.communities);
 
   if (error) return <p>ERROR</p>;
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <SmallSpinner />
   return (
     <ul className="flex flex-col gap-1">
       {communityData?.slice(0, 5).map((item, index) => {
