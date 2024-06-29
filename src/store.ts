b937aaef2db66c07782a8cabe35f185a4aa86f26
Sandro <vipsandro11@gmail.com>
2024-06-29 19:26:25 +0400

    update store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/user/authSlice";
-import modalReducer from "./slices/modals/modalSlice";
-import userDataReducer from "./slices/user/userDataSlice";
-import communitiesReducer from "./slices/community/communitySlice";
+import rootReducer from "./rootReducer";
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    userData: userDataReducer,
-    modals: modalReducer,
-    communities: communitiesReducer,
-  },
+  reducer: rootReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
