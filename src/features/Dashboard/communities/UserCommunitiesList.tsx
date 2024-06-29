3e404bf347ec516bd030d77fb6e785baaac8f969
Sandro <vipsandro11@gmail.com>
2024-06-29 19:11:50 +0400

    feat: create UserCommunitiesList.tsx

---
+++ src/features/Dashboard/communities/UserCommunitiesList.tsx
@@ -0,0 +1,31 @@
+import { useAppSelector } from "../../../hooks/reduxHooks";
+import SmallSpinner from "../../../ui/SmallSpinner";
+import SingleUserCommunityItem from "./SingleUserCommunityItem";
+
+const UserCommunitiesList = () => {
+  const {
+    communityData,
+    joinedGroups: { error, loading },
+  } = useAppSelector((store) => store.communities);
+
+  if (loading) return <SmallSpinner />;
+  if (error) return <p>error...</p>;
+
+  return (
+    <ul className="flex flex-col gap-3">
+      {communityData?.map((groupData) => {
+        const { banner, name, members } = groupData;
+        return (
+          <SingleUserCommunityItem
+            key={groupData.uid}
+            banner={banner}
+            name={name}
+            membersLength={members?.length ? members?.length : 0}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default UserCommunitiesList;
