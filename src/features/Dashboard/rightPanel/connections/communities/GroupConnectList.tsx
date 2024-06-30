be251ac269195847a5ce3c449d7d7a3eb19234ae
Sandro <vipsandro11@gmail.com>
2024-06-30 20:18:10 +0400

    update GroupConnectList.tsx

--- src/features/Dashboard/rightPanel/connections/communities/GroupConnectList.tsx
+++ src/features/Dashboard/rightPanel/connections/communities/GroupConnectList.tsx
@@ -1,6 +1,5 @@
-import { MdGroups } from "react-icons/md";
 import { CommunityTypes } from "../../../../../slices/community/communitySlice";
-import ConnectButton from "../../../../../ui/ConnectButton";
+import SingleNonJoinedCommunity from "./SingleNonJoinedCommunity";
 
 type GroupConnectListTypes = {
   nonJoinedGroupData: CommunityTypes[] | null;
@@ -10,25 +9,7 @@ const GroupConnectList = ({ nonJoinedGroupData }: GroupConnectListTypes) => {
   return (
     <ul className="flex flex-col gap-3">
       {nonJoinedGroupData?.map((group) => {
-        return (
-          <li key={group.uid} className="flex justify-between items-start">
-            <div className="flex gap-5">
-              <img
-                src={group.banner}
-                alt={group.name}
-                className="w-[2rem] h-[2rem] object-cover rounded-full bg-center"
-              />
-              <div>
-                <p className="text-[.9rem]">{group.name}</p>
-                <p className="flex gap-1 items-center text-[.8rem] text-gray-500">
-                  <MdGroups />
-                  {group.members ? group.members.length : 0}
-                </p>
-              </div>
-            </div>
-            <ConnectButton name="join" join />
-          </li>
-        );
+        return <SingleNonJoinedCommunity key={group.uid} group={group} />;
       })}
     </ul>
   );
