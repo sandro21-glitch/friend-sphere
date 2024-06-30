3cd87015121b6b188afcd10176c8db06a638c046
Sandro <vipsandro11@gmail.com>
2024-06-30 20:00:30 +0400

    fix(Dashboard): optimize fetching non-joined communities on path change

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import LeftPanel from "../features/Dashboard/leftPanel/LeftPanel";
 import RightPanel from "../features/Dashboard/rightPanel/RightPanel";
 import Modals from "../features/Dialogs/Modals";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { database } from "../config/firebase";
 import { onValue, ref } from "firebase/database";
 import { setUser } from "../slices/user/authSlice";
@@ -13,33 +13,40 @@ import { fetchNonJoinedCommunities } from "../slices/community/communityThunks";
 const Dashboard = () => {
   const dispatch = useAppDispatch();
   const { userData } = useAppSelector((state) => state.auth);
-  const path = useLocation();
-  console.log(path);
-
-  useEffect(() => {
-    const userRef = ref(database, `users/${userData?.uid}`);
-    const unsubscribe = onValue(userRef, (snapshot) => {
-      if (snapshot.exists()) {
-        dispatch(setUser(snapshot.val()));
-      } else {
-        console.log("No data available");
-      }
-    });
-    return () => unsubscribe();
-  }, [userData?.uid, dispatch]);
-
+  const location = useLocation();
+  const prevPathRef = useRef<string | null>(null);
 
   useEffect(() => {
     if (userData?.uid) {
-      dispatch(fetchNonJoinedCommunities(userData.uid));
+      const userRef = ref(database, `users/${userData.uid}`);
+      const unsubscribe = onValue(userRef, (snapshot) => {
+        if (snapshot.exists()) {
+          dispatch(setUser(snapshot.val()));
+        } else {
+          console.log("No data available");
+        }
+      });
+      return () => unsubscribe();
     }
-  }, [path, userData?.uid, dispatch]);
+  }, [userData?.uid, dispatch]);
+
+  // fetch non-joined communities when path changes but avoid unnecessary fetches
+  useEffect(() => {
+    if (userData?.uid) {
+      const currentPath = location.pathname;
+
+      if (prevPathRef.current !== currentPath) {
+        dispatch(fetchNonJoinedCommunities(userData.uid));
+        prevPathRef.current = currentPath;
+      }
+    }
+  }, [location.pathname, userData?.uid, dispatch]);
 
   return (
     <main className="relative bg-dashboard-bg">
       <Navbar />
       <section
-        className="section-center section-x grid grid-cols-4 gap-10 min-h-screen "
+        className="section-center section-x grid grid-cols-4 gap-10 min-h-screen"
         style={{ marginTop: "1rem" }}
       >
         <LeftPanel />
