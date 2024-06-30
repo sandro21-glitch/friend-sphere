6a32738783defc3532c7c47564fd269c42a276fc
Sandro <vipsandro11@gmail.com>
2024-06-30 20:18:16 +0400

    update ConnectButton.tsx

--- src/ui/ConnectButton.tsx
+++ src/ui/ConnectButton.tsx
@@ -4,12 +4,14 @@ type ConnectButtonTypes = {
   name: string;
   follow?: boolean;
   join?: boolean;
+  onClick?: () => void;
 };
 
-const ConnectButton = ({ name, follow, join }: ConnectButtonTypes) => {
+const ConnectButton = ({ name, follow, join, onClick }: ConnectButtonTypes) => {
   return (
     <button
       type="button"
+      onClick={onClick}
       className="border border-dashed border-azure-blue font-semibold text-azure-blue
      capitalize px-2 py-1 rounded-md flex items-center gap-2 text-[14px] hover:bg-azure-blue hover:text-white transition-all ease-in duration-150"
     >
