c2f841ecea5b7598949399a85c767ba0d96dff42
Sandro <vipsandro11@gmail.com>
2024-07-01 17:22:54 +0400

    update RightPanel.tsx

--- src/features/Dashboard/rightPanel/RightPanel.tsx
+++ src/features/Dashboard/rightPanel/RightPanel.tsx
@@ -1,9 +1,15 @@
+import { useLocation } from "react-router-dom";
 import Connections from "./Connections";
+import GroupInfo from "./GroupInfo";
 
 const RightPanel = () => {
+  const location = useLocation();
+  const isSingleCommunityPage = location.pathname.startsWith("/community");
+  const id = location.state?.id;
+
   return (
     <div className="bg-white col-span-1 h-[85vh] sticky top-[5rem] border rounded-md">
-      <Connections />
+      {isSingleCommunityPage ? <GroupInfo id={id} /> : <Connections />}
     </div>
   );
 };
