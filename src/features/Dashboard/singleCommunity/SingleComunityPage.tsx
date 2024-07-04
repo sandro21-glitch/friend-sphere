4f9cbcc99c5bd23a8e2edb4ed70c4f9e16cb6d83
Sandro <vipsandro11@gmail.com>
2024-07-04 19:45:01 +0400

    update SingleComunityPage.tsx

--- src/features/Dashboard/singleCommunity/SingleComunityPage.tsx
+++ src/features/Dashboard/singleCommunity/SingleComunityPage.tsx
@@ -1,13 +1,17 @@
 import { useLocation } from "react-router-dom";
+import { useAppSelector } from "../../../hooks/reduxHooks";
 
 const SingleComunityPage = () => {
   const location = useLocation();
   const { id } = location.state;
-  console.log(id);
+  const communityData = useAppSelector((store) =>
+    store.communities.communityData?.find((data) => data.uid === id)
+  );
 
+  console.log(communityData);
   return (
     <section className="col-span-2 min-h-full h-full mt-5">
-      SingleComunityPage
+      SingleComunityPage data
     </section>
   );
 };
