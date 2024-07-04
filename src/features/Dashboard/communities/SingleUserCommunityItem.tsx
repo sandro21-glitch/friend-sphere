167fcf95b56c1bbaa47e448bfdeb1a90faeae898
Sandro <vipsandro11@gmail.com>
2024-07-04 19:44:50 +0400

    update SingleUserCommunityItem.tsx

--- src/features/Dashboard/communities/SingleUserCommunityItem.tsx
+++ src/features/Dashboard/communities/SingleUserCommunityItem.tsx
@@ -4,24 +4,26 @@ type SingleCommunityTypes = {
   banner: string;
   name: string;
   membersLength: number;
+  id: string;
 };
 
 const SingleUserCommunityItem = ({
   banner,
   membersLength,
   name,
+  id,
 }: SingleCommunityTypes) => {
   return (
     <li className="w-full bg-white border rounded-md p-5 mb-5">
-      <Link to='/'>
+      <Link to={`/community/${name}`} state={{ id }} className="cursor-auto">
         <img
           src={banner}
           alt={name}
-          className="h-full w-full mb-4"
+          className="h-full w-full mb-4 cursor-pointer"
           loading="lazy"
         />
         <div>
-          <h3 className="font-semibold text-[1.5rem]">{name}</h3>
+          <h3 className="font-semibold text-[1.5rem] cursor-pointer">{name}</h3>
           <p className="text-gray-500">{membersLength} members</p>
         </div>
       </Link>
