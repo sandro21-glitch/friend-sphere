b1fe7c54d3510394852b589c4ac5e83c886a0ed7
Sandro <vipsandro11@gmail.com>
2024-07-04 16:31:14 +0400

    update modalSlice.ts

--- src/slices/modals/modalSlice.ts
+++ src/slices/modals/modalSlice.ts
@@ -3,14 +3,34 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 // import type { RootState } from "../store";
 
+interface joinCommunityTypes {
+  isModalOpen: boolean;
+  communityData: {
+    membersCount: number;
+    communityName: string;
+    communityId: string;
+  } | null;
+}
 interface ModalState {
   userProfileModal: boolean;
   updateProfileModal: boolean;
+  joinCommunity: {
+    isModalOpen: boolean;
+    communityData: {
+      membersCount: number;
+      communityName: string;
+      communityId: string;
+    } | null;
+  };
 }
 
 const initialState: ModalState = {
   userProfileModal: false,
   updateProfileModal: false,
+  joinCommunity: {
+    isModalOpen: false,
+    communityData: null,
+  },
 };
 
 export const modalSlice = createSlice({
@@ -23,10 +43,20 @@ export const modalSlice = createSlice({
     setUpdateProfileModal: (state, action: PayloadAction<boolean>) => {
       state.updateProfileModal = action.payload;
     },
+    setJoinCommunityModal: (
+      state,
+      action: PayloadAction<joinCommunityTypes>
+    ) => {
+      state.joinCommunity.isModalOpen = action.payload.isModalOpen;
+      state.joinCommunity.communityData = action.payload.communityData;
+    },
   },
 });
 
-export const { setUserProfileModal, setUpdateProfileModal } =
-  modalSlice.actions;
+export const {
+  setUserProfileModal,
+  setUpdateProfileModal,
+  setJoinCommunityModal,
+} = modalSlice.actions;
 
 export default modalSlice.reducer;
