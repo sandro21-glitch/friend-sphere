d210b02726c63ee737bae595684b6cc73cecb4e1
Sandro <vipsandro11@gmail.com>
2024-07-04 20:46:59 +0400

    update LeaveGroup.tsx

--- src/features/Dashboard/rightPanel/groupInfo/LeaveGroup.tsx
+++ src/features/Dashboard/rightPanel/groupInfo/LeaveGroup.tsx
@@ -1,22 +1,27 @@
 import { useAppDispatch, useAppSelector } from "../../../../hooks/reduxHooks";
-import { leaveGroup } from "../../../../slices/community/communityThunks";
+import { setLeaveCommunity } from "../../../../slices/modals/modalSlice";
 
 type LeaveGroupTypes = {
   groupId: string;
 };
 const LeaveGroup = ({ groupId }: LeaveGroupTypes) => {
   const uid = useAppSelector((store) => store.auth.userData?.uid);
-  const disaptch = useAppDispatch();
-  const handleLeaveGroup = () => {
+  const dispatch = useAppDispatch();
+  const handleOpenGroupPopup = () => {
     if (uid) {
-      disaptch(leaveGroup({ communityUid: groupId, uid }));
+      dispatch(
+        setLeaveCommunity({
+          isModalOpen: true,
+          dataIds: { communityId: groupId, userId: uid },
+        })
+      );
     }
   };
 
   return (
     <button
       type="button"
-      onClick={handleLeaveGroup}
+      onClick={handleOpenGroupPopup}
       className="text-[14px] text-center w-full
    border border-red-600 py-[4px] rounded-md text-red-600 hover:bg-red-500 hover:text-white 
    transition-colors ease-in duration-150 mb-3"
