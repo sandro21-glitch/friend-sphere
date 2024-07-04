07afa90cc5c555574b0e85a43d7ed562ee14d8ce
Sandro <vipsandro11@gmail.com>
2024-07-04 16:30:56 +0400

    update Modals.tsx

--- src/features/Dialogs/Modals.tsx
+++ src/features/Dialogs/Modals.tsx
@@ -1,15 +1,22 @@
 import { useAppSelector } from "../../hooks/reduxHooks";
 import EditProfilePopup from "./EditProfilePopup";
+import JoinCommunityPopup from "./joinCommunityPopup/JoinCommunityPopup";
 
 const Modals = () => {
-  const { updateProfileModal } = useAppSelector((store) => store.modals);
-  const isAnyModalOpen = updateProfileModal;
+  const { updateProfileModal, joinCommunity } = useAppSelector(
+    (store) => store.modals
+  );
+  const isAnyModalOpen = updateProfileModal || joinCommunity.isModalOpen;
   if (!isAnyModalOpen) return null;
   return (
     <div className="w-full h-full absolute inset-0 flex items-center justify-center z-[9999] min-h-screen">
       <div className="fixed inset-0 bg-black opacity-30"></div>
-      <div className="z-[9999] ">
-        {updateProfileModal ? <EditProfilePopup /> : null}
+      <div className="z-[9999]">
+        {updateProfileModal ? (
+          <EditProfilePopup />
+        ) : joinCommunity.isModalOpen ? (
+          <JoinCommunityPopup />
+        ) : null}
       </div>
     </div>
   );
