223ba5f36ebf6c0e2d140acd49440df2e3c4544a
Sandro <vipsandro11@gmail.com>
2024-07-04 16:30:59 +0400

    feat: create JoinCommunityPopup.tsx

---
+++ src/features/Dialogs/joinCommunityPopup/JoinCommunityPopup.tsx
@@ -0,0 +1,29 @@
+import { useAppSelector } from "../../../hooks/reduxHooks";
+import JoinCommunityPopupActions from "./JoinCommunityPopupActions";
+import JoinCommunityPopupHeader from "./JoinCommunityPopupHeader";
+
+const JoinCommunityPopup = () => {
+    const joinCommunity = useAppSelector((store) => store.modals.joinCommunity);
+
+  if (!joinCommunity.communityData) {
+    return null;
+  }
+
+  const { communityId, communityName, membersCount } = joinCommunity.communityData;
+
+  return (
+    <div className="w-full max-w-[28rem] h-auto bg-white p-5 flex justify-center flex-col items-center rounded-md">
+      <JoinCommunityPopupHeader
+        communityName={communityName}
+        membersCount={membersCount}
+      />
+      <p className="text-center mb-5">
+        Are you sure you want to join this community? You can always leave
+        later.
+      </p>
+      <JoinCommunityPopupActions communityId={communityId} />
+    </div>
+  );
+};
+
+export default JoinCommunityPopup;
