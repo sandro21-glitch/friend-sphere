f5c4a2cfd979c899fca79f9512aa6810c2a1dc79
Sandro <vipsandro11@gmail.com>
2024-07-05 20:05:06 +0400

    feat: create GroupHeader.tsx

---
+++ src/features/Dashboard/singleCommunityFeed/groupHeaderSection/GroupHeader.tsx
@@ -0,0 +1,33 @@
+type GroupHeaderTypes = {
+  postPage: string;
+  setPostPage: (page: string) => void;
+};
+
+const GroupHeader = ({ postPage, setPostPage }: GroupHeaderTypes) => {
+  return (
+    <ul className="flex items-center">
+      <li
+        onClick={() => setPostPage("all")}
+        className={`p-2 rounded-md w-full text-center cursor-pointer ${
+          postPage === "all"
+            ? "bg-azure-blue text-white"
+            : "border-b-2 border-transparent hover:border-gray-400 text-gray-500"
+        }`}
+      >
+        All posts
+      </li>
+      <li
+        onClick={() => setPostPage("following")}
+        className={`p-2 rounded-md w-full text-center cursor-pointer ${
+          postPage === "following"
+            ? "bg-azure-blue"
+            : "border-b-2 border-transparent hover:border-gray-400"
+        }`}
+      >
+        You're following
+      </li>
+    </ul>
+  );
+};
+
+export default GroupHeader;
