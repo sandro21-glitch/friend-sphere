ea69af4f90be46aea4b0688949a87d673ffde0a6
Sandro <vipsandro11@gmail.com>
2024-07-05 20:14:19 +0400

    update SingleComunityPage.tsx

--- src/features/Dashboard/singleCommunityFeed/SingleComunityPage.tsx
+++ src/features/Dashboard/singleCommunityFeed/SingleComunityPage.tsx
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import { useAppSelector } from "../../../hooks/reduxHooks";
 import GroupHeader from "./groupHeaderSection/GroupHeader";
 import { useState } from "react";
+import GroupPostForm from "./groupHeaderSection/GroupPostForm";
 
 const SingleComunityPage = () => {
   const [postPage, setPostPage] = useState("all");
@@ -13,8 +14,9 @@ const SingleComunityPage = () => {
 
   console.log(communityData);
   return (
-    <section className="col-span-2 min-h-full h-full mt-5">
+    <section className="col-span-2 min-h-full h-full mt-5 bg-white">
       <GroupHeader postPage={postPage} setPostPage={setPostPage} />
+      <GroupPostForm />
     </section>
   );
 };
