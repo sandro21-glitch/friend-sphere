6f5e5449a4c1ef84f22791799ad11490426f8a1c
Sandro <vipsandro11@gmail.com>
2024-07-06 19:50:21 +0400

    update rootReducer.ts

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -3,12 +3,14 @@ import authReducer from "./slices/user/authSlice";
 import modalReducer from "./slices/modals/modalSlice";
 import userDataReducer from "./slices/user/userDataSlice";
 import communitiesReducer from "./slices/community/communitySlice";
+import postsReducer from "./slices/posts/postsSlice";
 
 const appReducer = combineReducers({
   auth: authReducer,
   userData: userDataReducer,
   modals: modalReducer,
   communities: communitiesReducer,
+  posts: postsReducer,
 });
 
 const rootReducer = (
