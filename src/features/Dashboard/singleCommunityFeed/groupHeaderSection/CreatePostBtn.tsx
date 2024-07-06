0aecae4c53440f0680992c7035f1778e9cee379b
Sandro <vipsandro11@gmail.com>
2024-07-06 21:00:37 +0400

    feat: create CreatePostBtn.tsx

---
+++ src/features/Dashboard/singleCommunityFeed/groupHeaderSection/CreatePostBtn.tsx
@@ -0,0 +1,13 @@
+const CreatePostBtn = () => {
+  return (
+    <button
+      type="submit"
+      className="self-start bg-blue-500 hover:bg-deep-blue transition-colors ease-in duration-150
+      text-white py-1 px-4 text-[14px] rounded-md"
+    >
+      Create post
+    </button>
+  );
+};
+
+export default CreatePostBtn;
