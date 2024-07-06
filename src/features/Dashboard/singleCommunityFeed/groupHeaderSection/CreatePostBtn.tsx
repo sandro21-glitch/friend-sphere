02176a9ad292b30f683df44f4e94c12edeba7a2a
Sandro <vipsandro11@gmail.com>
2024-07-07 00:54:51 +0400

    update CreatePostBtn.tsx

--- src/features/Dashboard/singleCommunityFeed/groupHeaderSection/CreatePostBtn.tsx
+++ src/features/Dashboard/singleCommunityFeed/groupHeaderSection/CreatePostBtn.tsx
@@ -1,11 +1,20 @@
+import { useAppSelector } from "../../../../hooks/reduxHooks";
+
 const CreatePostBtn = () => {
+  const {
+    loading: { adding },
+    error: { addingError },
+  } = useAppSelector((store) => store.posts);
+
   return (
     <button
       type="submit"
       className="self-start bg-blue-500 hover:bg-deep-blue transition-colors ease-in duration-150
       text-white py-1 px-4 text-[14px] rounded-md"
+      disabled={adding}
     >
-      Create post
+      {adding ? "processing..." : "Create post"}
+      {addingError && addingError}
     </button>
   );
 };
