da95a694b1e71154cc76f8bdb3b0cd28a76865bc
Sandro <vipsandro11@gmail.com>
2024-07-06 21:00:33 +0400

    update SingleComunityPage.tsx

--- src/features/Dashboard/singleCommunityFeed/SingleComunityPage.tsx
+++ src/features/Dashboard/singleCommunityFeed/SingleComunityPage.tsx
@@ -1,8 +1,9 @@
 import { useLocation } from "react-router-dom";
-import { useAppSelector } from "../../../hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
 import GroupHeader from "./groupHeaderSection/GroupHeader";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GroupPostForm from "./groupHeaderSection/GroupPostForm";
+import { fetchCommunityPosts } from "../../../slices/posts/postThunks";
 
 const SingleComunityPage = () => {
   const [postPage, setPostPage] = useState("all");
@@ -11,12 +12,18 @@ const SingleComunityPage = () => {
   const communityData = useAppSelector((store) =>
     store.communities.communityData?.find((data) => data.uid === id)
   );
+  const dispatch = useAppDispatch();
+  if (!communityData) return null;
+  const { uid } = communityData;
 
-  console.log(communityData);
+  useEffect(() => {
+    dispatch(fetchCommunityPosts({ communityId: communityData.uid }));
+  }, [dispatch, postPage, uid]);
+
   return (
     <section className="col-span-2 min-h-full h-full mt-5 bg-white">
       <GroupHeader postPage={postPage} setPostPage={setPostPage} />
-      <GroupPostForm />
+      <GroupPostForm groupId={uid} />
     </section>
   );
 };
