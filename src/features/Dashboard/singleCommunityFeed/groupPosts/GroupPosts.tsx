0e623d89b8415aa69f9d4a7a8e868cff86e1589b
Sandro <vipsandro11@gmail.com>
2024-07-07 19:29:17 +0400

    update GroupPosts.tsx

--- src/features/Dashboard/singleCommunityFeed/groupPosts/GroupPosts.tsx
+++ src/features/Dashboard/singleCommunityFeed/groupPosts/GroupPosts.tsx
@@ -1,14 +1,23 @@
 import { useAppSelector } from "../../../../hooks/reduxHooks";
 import SingleGroupPost from "./SingleGroupPost";
 
-const GroupPosts = () => {
+type GroupPostTypes = {
+  communityId: string;
+};
+const GroupPosts = ({ communityId }: GroupPostTypes) => {
   const groupPosts = useAppSelector((store) => store.posts.communityPosts);
   if (!groupPosts) return <p>No posts</p>;
   return (
     <div className="py-5 px-2">
       <ul className="flex flex-col gap-5">
         {groupPosts?.map((post) => {
-          return <SingleGroupPost key={post.postId} post={post} />;
+          return (
+            <SingleGroupPost
+              key={post.postId}
+              post={post}
+              communityId={communityId}
+            />
+          );
         })}
       </ul>
     </div>
