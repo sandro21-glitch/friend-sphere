ef40670d5919e9b8b111de82997cb02fa4a74f49
Sandro <vipsandro11@gmail.com>
2024-07-07 19:09:05 +0400

    update SingleGroupPost.tsx

--- src/features/Dashboard/singleCommunityFeed/groupPosts/SingleGroupPost.tsx
+++ src/features/Dashboard/singleCommunityFeed/groupPosts/SingleGroupPost.tsx
@@ -1,6 +1,8 @@
-import { BiComment, BiLike } from "react-icons/bi";
 import { UserPostTypes } from "../../../../slices/posts/postsSlice";
 import { formatDistanceToNow, parseISO } from "date-fns";
+import PostHeader from "./PostHeader";
+import UserPost from "./UserPost";
+import PostActions from "./PostActions";
 
 type SinglePostTypes = {
   post: UserPostTypes;
@@ -15,35 +17,9 @@ const SingleGroupPost = ({ post }: SinglePostTypes) => {
 
   return (
     <li className="border rounded-md p-4">
-      <div className="flex justify-between items-start mb-3">
-        <div className="flex items-center">
-          <img
-            src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
-            alt="user img"
-            className="w-14 h-14 rounded-full"
-          />
-          <div className="leading-5">
-            <p className="font-semibold text-[18px]">{userName}</p>
-            <p className="text-[14px]">{groupName}</p>
-          </div>
-        </div>
-        <p className="text-[16px]">{timeAgo}</p>
-      </div>
-      <p className="mb-5">{userPost}</p>
-      <div className="flex items-center gap-4">
-        <button className="flex items-center gap-1">
-          <BiLike className=" text-[1.5rem]" />
-          <span className="text-[16px] font-semibold">
-            {likedBy?.length || 0}
-          </span>
-        </button>
-        <button className="flex items-center gap-1">
-          <BiComment className="text-[1.3rem]" />
-          <span className="text-[16px] font-semibold">
-            {postComments?.length || 0}
-          </span>
-        </button>
-      </div>
+      <PostHeader userName={userName} groupName={groupName} timeAgo={timeAgo} />
+      <UserPost userPost={userPost} />
+      <PostActions likedByLength={likedBy?.length || 0} postCommentLength={postComments?.length || 0} />
     </li>
   );
 };
