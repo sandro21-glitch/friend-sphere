0815b83e96476e8f5af7230b0c0c4c36b65cb70b
Sandro <vipsandro11@gmail.com>
2024-07-08 00:44:12 +0400

    update GroupPosts.tsx

--- src/features/Dashboard/singleCommunityFeed/groupPosts/GroupPosts.tsx
+++ src/features/Dashboard/singleCommunityFeed/groupPosts/GroupPosts.tsx
@@ -7,18 +7,22 @@ type GroupPostTypes = {
 const GroupPosts = ({ communityId }: GroupPostTypes) => {
   const groupPosts = useAppSelector((store) => store.posts.communityPosts);
   if (!groupPosts) return <p>No posts</p>;
+
   return (
     <div className="py-5 px-2">
       <ul className="flex flex-col gap-5">
-        {groupPosts?.map((post) => {
-          return (
-            <SingleGroupPost
-              key={post.postId}
-              post={post}
-              communityId={communityId}
-            />
-          );
-        })}
+        {groupPosts
+          .slice()
+          .reverse()
+          .map((post) => {
+            return (
+              <SingleGroupPost
+                key={post.postId}
+                post={post}
+                communityId={communityId}
+              />
+            );
+          })}
       </ul>
     </div>
   );
