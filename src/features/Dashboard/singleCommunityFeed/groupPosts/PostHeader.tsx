89611de0c2f1f4a760c8f436b4a9686f5bf7c009
Sandro <vipsandro11@gmail.com>
2024-07-07 19:09:02 +0400

    feat: create PostHeader.tsx

---
+++ src/features/Dashboard/singleCommunityFeed/groupPosts/PostHeader.tsx
@@ -0,0 +1,25 @@
+type PostHeaderTypes = {
+  userName: string;
+  groupName: string;
+  timeAgo: string;
+};
+const PostHeader = ({ groupName, timeAgo, userName }: PostHeaderTypes) => {
+  return (
+    <div className="flex justify-between items-start mb-3">
+      <div className="flex items-center">
+        <img
+          src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
+          alt="user img"
+          className="w-14 h-14 rounded-full"
+        />
+        <div className="leading-5">
+          <p className="font-semibold text-[18px]">{userName}</p>
+          <p className="text-[14px]">{groupName}</p>
+        </div>
+      </div>
+      <p className="text-[16px]">{timeAgo}</p>
+    </div>
+  );
+};
+
+export default PostHeader;
