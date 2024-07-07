f901fc957358d2e0509cf4860c9a6b6429e75d06
Sandro <vipsandro11@gmail.com>
2024-07-07 19:29:19 +0400

    update PostActions.tsx

--- src/features/Dashboard/singleCommunityFeed/groupPosts/PostActions.tsx
+++ src/features/Dashboard/singleCommunityFeed/groupPosts/PostActions.tsx
@@ -1,14 +1,34 @@
 import { BiComment, BiLike } from "react-icons/bi";
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
+import { likePost } from "../../../../slices/posts/postThunks";
 
 type PostActionTypes = {
   likedByLength: number;
   postCommentLength: number;
+  userId: string;
+  postId: string;
+  communityId: string;
 };
 
-const PostActions = ({ likedByLength, postCommentLength }: PostActionTypes) => {
+const PostActions = ({
+  likedByLength,
+  postCommentLength,
+  userId,
+  postId,
+  communityId,
+}: PostActionTypes) => {
+  const dispatch = useAppDispatch();
+  const handleLikePost = () => {
+    dispatch(likePost({ postId, userId, communityId }));
+  };
+
   return (
     <div className="flex items-center gap-4">
-      <button className="flex items-center gap-1">
+      <button
+        type="button"
+        onClick={handleLikePost}
+        className="flex items-center gap-1"
+      >
         <BiLike className=" text-[1.5rem]" />
         <span className="text-[16px] font-semibold">{likedByLength}</span>
       </button>
