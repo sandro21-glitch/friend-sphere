3d8010ea10a85b6340b17e665827e7dd8f3ad934
Sandro <vipsandro11@gmail.com>
2024-07-10 01:09:07 +0400

    update PostCommentText.tsx

--- src/features/Dashboard/postComments/PostCommentText.tsx
+++ src/features/Dashboard/postComments/PostCommentText.tsx
@@ -1,11 +1,9 @@
-const PostCommentText = () => {
-  return (
-    <p className="mb-5">
-      ygasdjhgasdjhg asgdhjkasg dasjhgd jhgasdhjg asdjhgsdafhjg
-      sadhjfgsjhadgfhjg safjhgsadjhfgsadjh gjhasdgfjkhsagd hjksgdfjhsgdafjh
-      gsdjfhgsdahjfg hjsadgfhjasdgf
-    </p>
-  );
+type PostTextTypes = {
+  userPost: string;
+};
+
+const PostCommentText = ({ userPost }: PostTextTypes) => {
+  return <p className="mb-5">{userPost}</p>;
 };
 
 export default PostCommentText;
