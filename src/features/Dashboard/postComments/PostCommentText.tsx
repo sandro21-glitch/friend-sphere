bf4d7ab8a13a55d3746bb9875ab651f0875b1cc4
Sandro <vipsandro11@gmail.com>
2024-07-09 21:46:21 +0400

    feat: create PostCommentText.tsx

---
+++ src/features/Dashboard/postComments/PostCommentText.tsx
@@ -0,0 +1,11 @@
+const PostCommentText = () => {
+  return (
+    <p className="mb-5">
+      ygasdjhgasdjhg asgdhjkasg dasjhgd jhgasdhjg asdjhgsdafhjg
+      sadhjfgsjhadgfhjg safjhgsadjhfgsadjh gjhasdgfjkhsagd hjksgdfjhsgdafjh
+      gsdjfhgsdahjfg hjsadgfhjasdgf
+    </p>
+  );
+};
+
+export default PostCommentText;
