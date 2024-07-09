feeec5b7c92fb9284bf9a44d9e96fcc4c7a59b8e
Sandro <vipsandro11@gmail.com>
2024-07-09 21:59:32 +0400

    update PostComments.tsx

--- src/features/Dashboard/postComments/PostComments.tsx
+++ src/features/Dashboard/postComments/PostComments.tsx
@@ -1,14 +1,16 @@
 import PostCommentActions from "./PostCommentActions";
+import PostCommentForm from "./PostCommentForm";
 import PostCommentsHeader from "./PostCommentsHeader";
 import PostCommentText from "./PostCommentText";
 
 const PostComments = () => {
   return (
-    <section className="col-span-2 min-h-full h-full mt-5 bg-white rounded-lg border">
+    <section className="col-span-2 max-h-[85vh] h-full mt-5 bg-white rounded-lg border">
       <div className="p-5">
         <PostCommentsHeader />
         <PostCommentText />
         <PostCommentActions />
+        <PostCommentForm />
       </div>
     </section>
   );
