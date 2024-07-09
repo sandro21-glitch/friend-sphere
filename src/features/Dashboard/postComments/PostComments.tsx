05e15fc46be0b65941ae94d5c979fb5778319032
Sandro <vipsandro11@gmail.com>
2024-07-10 01:09:03 +0400

    update PostComments.tsx

--- src/features/Dashboard/postComments/PostComments.tsx
+++ src/features/Dashboard/postComments/PostComments.tsx
@@ -1,14 +1,34 @@
+import { useLocation } from "react-router-dom";
 import PostCommentActions from "./PostCommentActions";
 import PostCommentForm from "./PostCommentForm";
 import PostCommentsHeader from "./PostCommentsHeader";
 import PostCommentText from "./PostCommentText";
+import { PostActionTypes } from "../singleCommunityFeed/groupPosts/PostActions";
 
 const PostComments = () => {
+  const location = useLocation();
+  const { postInfo }: { postInfo: PostActionTypes } = location.state || {};
+
+  const {
+    communityId,
+    groupName,
+    likedBy,
+    postCommentLength,
+    postId,
+    timeAgo,
+    userPost,
+    userName,
+  } = postInfo;
+
   return (
     <section className="col-span-2 max-h-[85vh] h-full mt-5 bg-white rounded-lg border">
       <div className="p-5">
-        <PostCommentsHeader />
-        <PostCommentText />
+        <PostCommentsHeader
+          groupName={groupName}
+          userName={userName}
+          timeAgo={timeAgo}
+        />
+        <PostCommentText userPost={userPost} />
         <PostCommentActions />
         <PostCommentForm />
       </div>
