bb6f89ea8205570e7a25cebd3cbcde18b3c32c5a
Sandro <vipsandro11@gmail.com>
2024-07-10 21:57:36 +0400

    update PostCommentActions.tsx

--- src/features/Dashboard/postComments/PostCommentActions.tsx
+++ src/features/Dashboard/postComments/PostCommentActions.tsx
@@ -1,19 +1,31 @@
 import { BiComment, BiLike } from "react-icons/bi";
-import { Link } from "react-router-dom";
 import { CiSaveDown2 } from "react-icons/ci";
 import ActionDropdown from "../../../ui/ActionDropdown";
-const PostCommentActions = () => {
+
+type PostCommentActionTypes = {
+  likedBy: string[];
+  postCommentLength: number;
+};
+
+const PostCommentActions = ({
+  likedBy,
+  postCommentLength,
+}: PostCommentActionTypes) => {
   return (
     <div className="flex items-center justify-between mb-5">
       <div className="flex items-center gap-5">
         <button type="button" className="flex items-center gap-1">
           <BiLike className=" text-[1.5rem]" />
-          <span className="text-[16px] font-semibold">5</span>
+          <span className="text-[16px] font-semibold">
+            {likedBy.length || 0}
+          </span>
         </button>
-        <Link to={`/post/3}`} className="flex items-center gap-1">
+        <div className="flex items-center gap-1">
           <BiComment className="text-[1.3rem]" />
-          <span className="text-[16px] font-semibold">9</span>
-        </Link>
+          <span className="text-[16px] font-semibold">
+            {postCommentLength || 0}
+          </span>
+        </div>
       </div>
       <button type="button" className="flex items-center gap-1 relative group">
         <CiSaveDown2 className=" text-[1.5rem]" />
