cba4eb88732ca2fe9662a015c43609d21e76fa44
Sandro <vipsandro11@gmail.com>
2024-07-11 21:41:04 +0400

    update PostActions.tsx

--- src/features/Dashboard/singleCommunityFeed/groupPosts/PostActions.tsx
+++ src/features/Dashboard/singleCommunityFeed/groupPosts/PostActions.tsx
@@ -1,8 +1,6 @@
-import { BiComment, BiLike, BiSolidLike } from "react-icons/bi";
-import { useAppDispatch, useAppSelector } from "../../../../hooks/reduxHooks";
-import { likePost } from "../../../../slices/posts/postThunks";
-import { useState } from "react";
+import { BiComment } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import LikeButton from "../../../../ui/LikeButton";
 
 export interface PostActionTypes {
   likedBy: string[];
@@ -25,48 +23,9 @@ const PostActions = ({
   groupName,
   userPost,
 }: PostActionTypes) => {
-  const {
-    loading: { liking },
-  } = useAppSelector((store) => store.posts);
-  const userId = useAppSelector((store) => store.auth.userData?.uid);
-  const dispatch = useAppDispatch();
-
-  const [localLiked, setLocalLiked] = useState(
-    likedBy.some((likedId) => likedId === userId)
-  );
-  const [localLikedCount, setLocalLikedCount] = useState(likedBy.length);
-
-  const handleLikePost = async () => {
-    if (!userId) return;
-
-    setLocalLiked(!localLiked);
-    setLocalLikedCount(localLiked ? localLikedCount - 1 : localLikedCount + 1);
-
-    try {
-      await dispatch(likePost({ postId, userId, communityId })).unwrap();
-    } catch (error) {
-      setLocalLiked(!localLiked);
-      setLocalLikedCount(
-        localLiked ? localLikedCount + 1 : localLikedCount - 1
-      );
-    }
-  };
-
   return (
     <div className="flex items-center gap-4">
-      <button
-        type="button"
-        onClick={handleLikePost}
-        className="flex items-center gap-1"
-        disabled={liking}
-      >
-        {localLiked ? (
-          <BiSolidLike className=" text-[1.5rem]" />
-        ) : (
-          <BiLike className=" text-[1.5rem]" />
-        )}
-        <span className="text-[16px] font-semibold">{localLikedCount}</span>
-      </button>
+      <LikeButton likedBy={likedBy} postId={postId} communityId={communityId} />
       <Link
         to={`/post/${postId}`}
         state={{
