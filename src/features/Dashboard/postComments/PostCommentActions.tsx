342f9409cf262f8b56c1420ec49a10f411572b61
Sandro <vipsandro11@gmail.com>
2024-07-13 23:04:35 +0400

    update PostCommentActions.tsx

--- src/features/Dashboard/postComments/PostCommentActions.tsx
+++ src/features/Dashboard/postComments/PostCommentActions.tsx
@@ -3,6 +3,8 @@ import { CiSaveDown2 } from "react-icons/ci";
 import ActionDropdown from "../../../ui/ActionDropdown";
 
 import LikeButton from "../../../ui/LikeButton";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
+import { savePostThunk } from "../../../slices/posts/postThunks";
 
 type PostCommentActionTypes = {
   likedBy: string[];
@@ -17,6 +19,15 @@ const PostCommentActions = ({
   postId,
   communityId,
 }: PostCommentActionTypes) => {
+  const userId = useAppSelector((store) => store.auth.userData?.uid);
+
+  const dispatch = useAppDispatch();
+  const handleSavePost = () => {
+    if (userId) {
+      dispatch(savePostThunk({ communityId, postId, userId }));
+    }
+  };
+
   return (
     <div className="flex items-center justify-between mb-5">
       <div className="flex items-center gap-5">
@@ -32,7 +43,7 @@ const PostCommentActions = ({
           </span>
         </div>
       </div>
-      <button type="button" className="flex items-center gap-1 relative group">
+      <button type="button" onClick={handleSavePost} className="flex items-center gap-1 relative group">
         <CiSaveDown2 className=" text-[1.5rem]" />
         <ActionDropdown dropdownText="Save post" />
       </button>
