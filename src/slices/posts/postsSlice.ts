2e0c58cd60764fea6ff88f7cf9511a7d311a7a25
Sandro <vipsandro11@gmail.com>
2024-07-13 23:04:43 +0400

    update postsSlice.ts

--- src/slices/posts/postsSlice.ts
+++ src/slices/posts/postsSlice.ts
@@ -1,190 +1,234 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {
-  addCommentToPost,
-  addPostToCommunity,
-  fetchCommunityPosts,
-  likePost,
-  removePost,
-} from "./postThunks";
+  import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+  import {
+    addCommentToPost,
+    addPostToCommunity,
+    fetchCommunityPosts,
+    fetchSavedPostsThunk,
+    likePost,
+    removePost,
+    savePostThunk,
+  } from "./postThunks";
 
-export interface UserPostTypes {
-  userName: string;
-  postId: string;
-  userId: string;
-  userPost: string;
-  likedBy: string[] | null;
-  postComments:
-    | {
-        userComment: string;
-        userId: string;
-        userName: string;
-        postedAt?: string;
-      }[]
-    | null;
-  createdAt: string;
-  groupName: string;
-}
+  export interface UserPostTypes {
+    userName: string;
+    postId: string;
+    userId: string;
+    userPost: string;
+    likedBy: string[] | null;
+    postComments:
+      | {
+          userComment: string;
+          userId: string;
+          userName: string;
+          postedAt?: string;
+        }[]
+      | null;
+    createdAt: string;
+    groupName: string;
+  }
 
-interface PostsState {
-  communityPosts: UserPostTypes[] | null;
-  loading: {
-    fetching: boolean;
-    adding: boolean;
-    liking: boolean;
-    removing: boolean;
-    commenting: boolean;
+  interface PostsState {
+    communityPosts: UserPostTypes[] | null;
+    savedPosts: UserPostTypes[] | null;
+    loading: {
+      fetching: boolean;
+      adding: boolean;
+      liking: boolean;
+      removing: boolean;
+      commenting: boolean;
+      saving: boolean;
+      fetchingSavedPosts: boolean;
+    };
+    error: {
+      fetchingError: string | null;
+      addingError: string | null;
+      likingError: string | null;
+      removingError: string | null;
+      commentingError: string | null;
+      savingError: string | null;
+      fetchingSavedPostsError: string | null;
+    };
+  }
+  [];
+
+  const initialState: PostsState = {
+    communityPosts: null,
+    savedPosts: null,
+    loading: {
+      fetching: false,
+      adding: false,
+      liking: false,
+      removing: false,
+      commenting: false,
+      saving: false,
+      fetchingSavedPosts: false,
+    },
+    error: {
+      fetchingError: null,
+      addingError: null,
+      likingError: null,
+      removingError: null,
+      commentingError: null,
+      savingError: null,
+      fetchingSavedPostsError: null,
+    },
   };
-  error: {
-    fetchingError: string | null;
-    addingError: string | null;
-    likingError: string | null;
-    removingError: string | null;
-    commentingError: string | null;
-  };
-}
-[];
 
-const initialState: PostsState = {
-  communityPosts: null,
-  loading: {
-    fetching: false,
-    adding: false,
-    liking: false,
-    removing: false,
-    commenting: false,
-  },
-  error: {
-    fetchingError: null,
-    addingError: null,
-    likingError: null,
-    removingError: null,
-    commentingError: null,
-  },
-};
-
-export const postsSlice = createSlice({
-  name: "posts",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    //add post
-    builder
-      .addCase(addPostToCommunity.pending, (state) => {
-        state.loading.adding = true;
-      })
-      .addCase(addPostToCommunity.fulfilled, (state, action) => {
-        state.loading.adding = false;
-        if (state.communityPosts) {
-          const newPost = {
-            ...action.payload.post,
-            createdAt: new Date().toISOString(),
-          };
-          state.communityPosts.push(newPost);
-        }
-      })
-      .addCase(addPostToCommunity.rejected, (state, action) => {
-        state.loading.adding = false;
-        state.error.addingError = action.error.message || "Failed to add post";
-      });
-    //fetch all single group posts
-    builder
-      .addCase(fetchCommunityPosts.pending, (state) => {
-        state.loading.fetching = true;
-      })
-      .addCase(
-        fetchCommunityPosts.fulfilled,
-        (state, action: PayloadAction<UserPostTypes[]>) => {
+  export const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      //add post
+      builder
+        .addCase(addPostToCommunity.pending, (state) => {
+          state.loading.adding = true;
+        })
+        .addCase(addPostToCommunity.fulfilled, (state, action) => {
+          state.loading.adding = false;
+          if (state.communityPosts) {
+            const newPost = {
+              ...action.payload.post,
+              createdAt: new Date().toISOString(),
+            };
+            state.communityPosts.push(newPost);
+          }
+        })
+        .addCase(addPostToCommunity.rejected, (state, action) => {
+          state.loading.adding = false;
+          state.error.addingError = action.error.message || "Failed to add post";
+        });
+      //fetch all single group posts
+      builder
+        .addCase(fetchCommunityPosts.pending, (state) => {
+          state.loading.fetching = true;
+        })
+        .addCase(
+          fetchCommunityPosts.fulfilled,
+          (state, action: PayloadAction<UserPostTypes[]>) => {
+            state.loading.fetching = false;
+            state.communityPosts = action.payload || null;
+          }
+        )
+        .addCase(fetchCommunityPosts.rejected, (state, action) => {
           state.loading.fetching = false;
-          state.communityPosts = action.payload || null;
-        }
-      )
-      .addCase(fetchCommunityPosts.rejected, (state, action) => {
-        state.loading.fetching = false;
-        state.error.fetchingError = action.payload || "fetching posts rejected";
-      });
-    //like posts
-    builder
-      .addCase(likePost.pending, (state) => {
-        state.loading.liking = true;
-      })
-      .addCase(likePost.fulfilled, (state, action) => {
-        state.loading.liking = false;
-        const { postId, userId } = action.payload;
-        if (state.communityPosts) {
-          state.communityPosts = state.communityPosts?.map((post) =>
-            post.postId === postId
-              ? {
+          state.error.fetchingError = action.payload || "fetching posts rejected";
+        });
+      //like posts
+      builder
+        .addCase(likePost.pending, (state) => {
+          state.loading.liking = true;
+        })
+        .addCase(likePost.fulfilled, (state, action) => {
+          state.loading.liking = false;
+          const { postId, userId } = action.payload;
+          if (state.communityPosts) {
+            state.communityPosts = state.communityPosts?.map((post) =>
+              post.postId === postId
+                ? {
+                    ...post,
+                    likedBy: post.likedBy
+                      ? post.likedBy.includes(userId)
+                        ? post.likedBy.filter((id) => id !== userId) // remove userId if already liked
+                        : [...post.likedBy, userId] // add userId if not liked
+                      : [userId],
+                  }
+                : post
+            );
+          }
+        })
+        .addCase(likePost.rejected, (state, action) => {
+          state.loading.liking = false;
+          state.error.likingError = action.payload
+            ? action.payload.toString()
+            : "Unknown error";
+          alert(state.error.likingError || "Unknown error");
+        });
+      //remove post
+      builder
+        .addCase(removePost.pending, (state) => {
+          state.loading.removing = true;
+        })
+        .addCase(removePost.fulfilled, (state, action) => {
+          state.loading.removing = false;
+          const { postId } = action.payload;
+          if (state.communityPosts) {
+            state.communityPosts = state.communityPosts.filter(
+              (post) => post.postId !== postId
+            );
+          }
+        })
+        .addCase(removePost.rejected, (state, action) => {
+          state.loading.removing = false;
+          state.error.removingError = action.payload
+            ? action.payload.toString()
+            : "Unknown error";
+          alert(state.error.removingError || "Unknown error");
+        });
+      //comment to post
+      builder
+        .addCase(addCommentToPost.pending, (state) => {
+          state.loading.commenting = true;
+        })
+        .addCase(addCommentToPost.fulfilled, (state, action) => {
+          state.loading.commenting = false;
+          const { comment, communityId, postId } = action.payload;
+
+          if (state.communityPosts) {
+            state.communityPosts = state.communityPosts.map((post) => {
+              if (post.postId === postId) {
+                return {
                   ...post,
-                  likedBy: post.likedBy
-                    ? post.likedBy.includes(userId)
-                      ? post.likedBy.filter((id) => id !== userId) // remove userId if already liked
-                      : [...post.likedBy, userId] // add userId if not liked
-                    : [userId],
-                }
-              : post
-          );
-        }
-      })
-      .addCase(likePost.rejected, (state, action) => {
-        state.loading.liking = false;
-        state.error.likingError = action.payload
-          ? action.payload.toString()
-          : "Unknown error";
-        alert(state.error.likingError || "Unknown error");
-      });
-    //remove post
-    builder
-      .addCase(removePost.pending, (state) => {
-        state.loading.removing = true;
-      })
-      .addCase(removePost.fulfilled, (state, action) => {
-        state.loading.removing = false;
-        const { postId } = action.payload;
-        if (state.communityPosts) {
-          state.communityPosts = state.communityPosts.filter(
-            (post) => post.postId !== postId
-          );
-        }
-      })
-      .addCase(removePost.rejected, (state, action) => {
-        state.loading.removing = false;
-        state.error.removingError = action.payload
-          ? action.payload.toString()
-          : "Unknown error";
-        alert(state.error.removingError || "Unknown error");
-      });
-    //comment to post
-    builder
-      .addCase(addCommentToPost.pending, (state) => {
-        state.loading.commenting = true;
-      })
-      .addCase(addCommentToPost.fulfilled, (state, action) => {
-        state.loading.commenting = false;
-        const { comment, communityId, postId } = action.payload;
+                  postComments: [...(post.postComments || []), comment],
+                };
+              }
+              return post;
+            });
+          }
 
-        if (state.communityPosts) {
-          state.communityPosts = state.communityPosts.map((post) => {
-            if (post.postId === postId) {
-              return {
-                ...post,
-                postComments: [...(post.postComments || []), comment],
-              };
-            }
-            return post;
+          console.log(comment, communityId, postId);
+        })
+        .addCase(addCommentToPost.rejected, (state, action) => {
+          state.error.removingError = action.payload
+            ? action.payload.toString()
+            : "Unknown error";
+          alert(state.error.removingError || "Unknown error");
+        });
+      //save posts
+      builder
+        .addCase(savePostThunk.pending, (state) => {
+          state.loading.saving = true;
+        })
+        .addCase(savePostThunk.fulfilled, (state) => {
+          state.loading.saving = false;
+          console.log("fulfile");
+        })
+        .addCase(savePostThunk.rejected, (state, action) => {
+          state.loading.saving = false;
+          state.error.removingError = action.payload
+            ? action.payload.toString()
+            : "Unknown error";
+          alert(state.error.removingError || "Unknown error");
+        });
+        //fetch saved posts
+        builder
+          .addCase(fetchSavedPostsThunk.pending, (state) => {
+            state.loading.fetchingSavedPosts = true;
+          })
+          .addCase(fetchSavedPostsThunk.fulfilled, (state,action) => {
+            state.loading.fetchingSavedPosts = false;
+            console.log(action.payload); 
+          })
+          .addCase(fetchSavedPostsThunk.rejected, (state, action) => {
+            state.loading.fetchingSavedPosts = false;
+            state.error.fetchingSavedPostsError = action.payload
+              ? action.payload.toString()
+              : "Unknown error";
+            alert(state.error.removingError || "Unknown error");
           });
-        }
+    },
+  });
 
-        console.log(comment, communityId, postId);
-      })
-      .addCase(addCommentToPost.rejected, (state, action) => {
-        state.error.removingError = action.payload
-          ? action.payload.toString()
-          : "Unknown error";
-        alert(state.error.removingError || "Unknown error");
-      });
-  },
-});
+  export const {} = postsSlice.actions;
 
-export const {} = postsSlice.actions;
-
-export default postsSlice.reducer;
+  export default postsSlice.reducer;
