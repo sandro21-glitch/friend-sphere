60061be516773c5a579262a0eb38e6539866b833
Sandro <vipsandro11@gmail.com>
2024-07-13 23:04:37 +0400

    update SavedPosts.tsx

--- src/features/Dashboard/saved/SavedPosts.tsx
+++ src/features/Dashboard/saved/SavedPosts.tsx
@@ -1,10 +1,35 @@
-import DashboardPage from "../../../ui/DashboardPage"
-
+import { useEffect } from "react";
+import DashboardPage from "../../../ui/DashboardPage";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
+import { fetchSavedPostsThunk } from "../../../slices/posts/postThunks";
+import PageLoader from "../../../ui/PageLoader";
 
 const SavedPosts = () => {
-  return (
-    <DashboardPage>SavedPosts</DashboardPage>
-  )
-}
+  const userId = useAppSelector((store) => store.auth.userData?.uid);
+  const dispatch = useAppDispatch();
 
-export default SavedPosts
+  const {
+    error: { fetchingSavedPostsError },
+    loading: { fetchingSavedPosts },
+  } = useAppSelector((store) => store.posts);
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(fetchSavedPostsThunk({ userId }));
+    }
+  }, [dispatch, userId]);
+
+  if (fetchingSavedPostsError) return <p>Error..</p>;
+  if (fetchingSavedPosts)
+    return (
+      <DashboardPage>
+        <div className="flex items-center justify-center h-full">
+          <PageLoader />
+        </div>
+      </DashboardPage>  
+    );
+
+  return <DashboardPage>SavedPosts</DashboardPage>;
+};
+
+export default SavedPosts;
