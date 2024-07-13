b8ae3162abbeee142ed0bd0692c5dd3da38d4bc6
Sandro <vipsandro11@gmail.com>
2024-07-13 23:45:11 +0400

    feat: create SavedPostsList.tsx

---
+++ src/features/Dashboard/saved/SavedPostsList.tsx
@@ -0,0 +1,18 @@
+import { UserPostTypes } from "../../../slices/posts/postsSlice";
+import SingleSavedPost from "./SingleSavedPost";
+
+type SavedPostTypes = {
+  savedPosts: UserPostTypes[];
+};
+
+const SavedPostsList = ({ savedPosts }: SavedPostTypes) => {
+  return (
+    <ul className="p-4">
+      {savedPosts.map((post) => {
+        return <SingleSavedPost key={post.postId} post={post} />;
+      })}
+    </ul>
+  );
+};
+
+export default SavedPostsList;
