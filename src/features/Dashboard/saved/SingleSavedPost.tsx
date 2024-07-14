1793687a2d4815cb043b783f4eb6a0657f778c46
Sandro <vipsandro11@gmail.com>
2024-07-14 18:33:43 +0400

    update SingleSavedPost.tsx

--- src/features/Dashboard/saved/SingleSavedPost.tsx
+++ src/features/Dashboard/saved/SingleSavedPost.tsx
@@ -1,25 +1,15 @@
 import { formatDistanceToNow, parseISO } from "date-fns";
 import { UserPostTypes } from "../../../slices/posts/postsSlice";
-import LikeButton from "../../../ui/LikeButton";
-import { Link } from "react-router-dom";
-import RemovePostBtn from "../singleCommunityFeed/groupPosts/RemovePostBtn";
-import { BiComment } from "react-icons/bi";
+import SavedPostHeader from "./singleSavedPost/SavedPostHeader";
+import SavedPostText from "./singleSavedPost/SavedPostText";
+import SavedPostActions from "./singleSavedPost/SavedPostActions";
 
 type SingleSavedPostTypes = {
   post: UserPostTypes;
 };
 
 const SingleSavedPost = ({
-  post: {
-    createdAt,
-    groupName,
-    likedBy,
-    postComments,
-    postId,
-    userId,
-    userName,
-    userPost,
-  },
+  post: { createdAt, groupName, likedBy, postId, userId, userName, userPost },
 }: SingleSavedPostTypes) => {
   const parsedDate = parseISO(createdAt);
 
@@ -28,56 +18,23 @@ const SingleSavedPost = ({
 
   return (
     <li className="border rounded-md p-4 hover:shadow-lg transition-shadow ease-in duration-200">
-      <div className="flex justify-between items-start mb-3">
-        <div className="flex items-center">
-          <img
-            src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
-            alt="user img"
-            className="w-14 h-14 rounded-full"
-          />
-          <div className="leading-5">
-            <p className="font-semibold text-[18px]">{userName}</p>
-            <p className="text-[14px]">{groupName}</p>
-          </div>
-        </div>
-        <p className="text-[14px] text-gray-500">
-          {timeAgo.replace("about", " ")}
-        </p>
-      </div>
-      <p className="mb-5">{userPost}</p>;
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-4">
-          <LikeButton
-            likedBy={likedBy || []}
-            postId={postId}
-            communityId={"test"}
-          />
-          <Link
-            to={`/post/${postId}`}
-            state={{
-              postInfo: {
-                likedBy,
-                postCommentLength: 5,
-                postId,
-                communityId: "test",
-                timeAgo,
-                userName,
-                groupName,
-                userPost,
-              },
-            }}
-            className="flex items-center gap-1"
-          >
-            <BiComment className="text-[1.3rem]" />
-            <span className="text-[16px] font-semibold">{5}</span>
-          </Link>
-        </div>
-        <RemovePostBtn
-          postUserId={userId}
-          communityId={"test"}
-          postId={postId}
-        />
-      </div>
+      <SavedPostHeader
+        userName={userName}
+        groupName={groupName}
+        timeAgo={timeAgo}
+      />
+      <SavedPostText userPost={userPost} />
+      <SavedPostActions
+        likedBy={likedBy || []}
+        postCommentLength={1}
+        postId={postId}
+        communityId="test"
+        timeAgo={timeAgo}
+        userName={userName}
+        groupName={groupName}
+        userPost={userPost}
+        userId={userId}
+      />
     </li>
   );
 };
