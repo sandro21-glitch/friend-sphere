61affa613de35b83245dfa02f64d765543a2dcf7
Sandro <vipsandro11@gmail.com>
2024-07-14 22:50:05 +0400

    update SingleSavedPost.tsx

--- src/features/Dashboard/saved/SingleSavedPost.tsx
+++ src/features/Dashboard/saved/SingleSavedPost.tsx
@@ -1,15 +1,25 @@
 import { formatDistanceToNow, parseISO } from "date-fns";
-import { UserPostTypes } from "../../../slices/posts/postsSlice";
+import { SavedPostTypes } from "../../../slices/posts/postsSlice";
 import SavedPostHeader from "./singleSavedPost/SavedPostHeader";
 import SavedPostText from "./singleSavedPost/SavedPostText";
 import SavedPostActions from "./singleSavedPost/SavedPostActions";
 
 type SingleSavedPostTypes = {
-  post: UserPostTypes;
+  post: SavedPostTypes;
 };
 
 const SingleSavedPost = ({
-  post: { createdAt, groupName, likedBy, postId, userId, userName, userPost },
+  post: {
+    createdAt,
+    groupName,
+    likedBy,
+    postId,
+    userId,
+    userName,
+    userPost,
+    postComments,
+    communityId,
+  },
 }: SingleSavedPostTypes) => {
   const parsedDate = parseISO(createdAt);
 
@@ -26,9 +36,9 @@ const SingleSavedPost = ({
       <SavedPostText userPost={userPost} />
       <SavedPostActions
         likedBy={likedBy || []}
-        postCommentLength={1}
+        postCommentLength={postComments?.length || 0}
         postId={postId}
-        communityId="test"
+        communityId={communityId}
         timeAgo={timeAgo}
         userName={userName}
         groupName={groupName}
