55392e06a02b99aa05a00c4eb9e210b2d3ed8860
Sandro <vipsandro11@gmail.com>
2024-07-14 18:33:51 +0400

    feat: create LikePostAction.tsx

---
+++ src/features/Dashboard/saved/singleSavedPost/post actions/LikePostAction.tsx
@@ -0,0 +1,14 @@
+import LikeButton from "../../../../../ui/LikeButton";
+
+type LikePostActionTypes = {
+  likedBy: string[];
+  postId: string;
+  communityId: string;
+};
+const LikePostAction = ({ likedBy, postId,communityId }: LikePostActionTypes) => {
+  return (
+    <LikeButton likedBy={likedBy || []} postId={postId} communityId={communityId} />
+  );
+};
+
+export default LikePostAction;
