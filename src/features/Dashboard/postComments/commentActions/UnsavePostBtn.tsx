aac0819a41fe70aeadf9a3e2034faa4885a2f881
Sandro <vipsandro11@gmail.com>
2024-07-15 22:02:02 +0400

    update UnsavePostBtn.tsx

--- src/features/Dashboard/postComments/commentActions/UnsavePostBtn.tsx
+++ src/features/Dashboard/postComments/commentActions/UnsavePostBtn.tsx
@@ -1,9 +1,14 @@
 import { BsBookmarkDashFill } from "react-icons/bs";
+import ActionDropdown from "../../../../ui/ActionDropdown";
 
 const UnsavePostBtn = () => {
   return (
-    <button className="text-[1.7rem]">
-      <BsBookmarkDashFill />
+    <button type="button" className="flex items-center gap-5 relative group">
+      <BsBookmarkDashFill className="text-[1.7rem]" />
+      <ActionDropdown
+        dropdownText="Remove from saved"
+        positions="-top-10 right-[-155%]"
+      />
     </button>
   );
 };
