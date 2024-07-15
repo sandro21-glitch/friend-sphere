128b119be05ba7bcc928b5369051e61ae454600e
Sandro <vipsandro11@gmail.com>
2024-07-15 22:02:04 +0400

    update ActionDropdown.tsx

--- src/ui/ActionDropdown.tsx
+++ src/ui/ActionDropdown.tsx
@@ -2,15 +2,17 @@ import { GoTriangleDown } from "react-icons/go";
 
 type ActionDropdownTypes = {
   dropdownText: string;
+  positions?: string;
 };
 
-const ActionDropdown = ({ dropdownText }: ActionDropdownTypes) => {
+const ActionDropdown = ({ dropdownText, positions }: ActionDropdownTypes) => {
   return (
     <span
-      className="absolute -top-10 -right-[18px] group-hover:block hidden w-fit text-nowrap
+      className={`absolute ${
+        positions ? positions : "-top-10 -right-[18px]"
+      }  group-hover:block hidden w-fit text-nowrap
       text-[11px] bg-azure-blue px-1 py-2 rounded-md
-       text-white opacity-0 group-hover:opacity-100 transition-all ease-in duration-200
-      "
+       text-white opacity-0 group-hover:opacity-100 transition-all ease-in duration-200`}
     >
       {dropdownText}
       <GoTriangleDown
