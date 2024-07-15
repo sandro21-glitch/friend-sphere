a688a6851808571721d543e0f18a1795ba5afb04
Sandro <vipsandro11@gmail.com>
2024-07-15 21:53:11 +0400

    update ActionDropdown.tsx

--- src/ui/ActionDropdown.tsx
+++ src/ui/ActionDropdown.tsx
@@ -7,7 +7,7 @@ type ActionDropdownTypes = {
 const ActionDropdown = ({ dropdownText }: ActionDropdownTypes) => {
   return (
     <span
-      className="absolute -top-9 -right-[18px] group-hover:block hidden w-fit text-nowrap
+      className="absolute -top-10 -right-[18px] group-hover:block hidden w-fit text-nowrap
       text-[11px] bg-azure-blue px-1 py-2 rounded-md
        text-white opacity-0 group-hover:opacity-100 transition-all ease-in duration-200
       "
