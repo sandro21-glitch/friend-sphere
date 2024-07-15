b44cc113db26dc710d94772e4fb4fc5750ce7528
Sandro <vipsandro11@gmail.com>
2024-07-15 21:53:09 +0400

    feat: create UnsavePostBtn.tsx

---
+++ src/features/Dashboard/postComments/commentActions/UnsavePostBtn.tsx
@@ -0,0 +1,11 @@
+import { BsBookmarkDashFill } from "react-icons/bs";
+
+const UnsavePostBtn = () => {
+  return (
+    <button className="text-[1.7rem]">
+      <BsBookmarkDashFill />
+    </button>
+  );
+};
+
+export default UnsavePostBtn;
