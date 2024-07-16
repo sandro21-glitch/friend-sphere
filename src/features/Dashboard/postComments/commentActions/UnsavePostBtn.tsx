66a1700f7b4eae7ac4e0effaf1716f4723d09ef9
Sandro <vipsandro11@gmail.com>
2024-07-16 20:37:06 +0400

    update UnsavePostBtn.tsx

--- src/features/Dashboard/postComments/commentActions/UnsavePostBtn.tsx
+++ src/features/Dashboard/postComments/commentActions/UnsavePostBtn.tsx
@@ -1,9 +1,35 @@
 import { BsBookmarkDashFill } from "react-icons/bs";
 import ActionDropdown from "../../../../ui/ActionDropdown";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/reduxHooks";
+import { unsavePostThunk } from "../../../../slices/posts/postThunks";
 
-const UnsavePostBtn = () => {
+type UnsavePostBtnTypes = {
+  postId: string;
+  communityId: string;
+  setIsPostSaved: (isSaved: boolean) => void;
+};
+
+const UnsavePostBtn = ({
+  communityId,
+  postId,
+  setIsPostSaved,
+}: UnsavePostBtnTypes) => {
+  const userId = useAppSelector((store) => store.auth.userData?.uid);
+  const dispatch = useAppDispatch();
+
+  const handleUnsavePost = () => {
+    if (userId) {
+      dispatch(unsavePostThunk({ communityId, postId, userId }));
+      setIsPostSaved(false); // optimistically update UI
+    }
+  };
+
   return (
-    <button type="button" className="flex items-center gap-5 relative group">
+    <button
+      type="button"
+      onClick={handleUnsavePost}
+      className="flex items-center gap-5 relative group"
+    >
       <BsBookmarkDashFill className="text-[1.7rem]" />
       <ActionDropdown
         dropdownText="Remove from saved"
