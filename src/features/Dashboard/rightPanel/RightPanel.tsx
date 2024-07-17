a8d3b43d7d3d3fdf68bdd24bc5ce58bbac4e9d91
Sandro <vipsandro11@gmail.com>
2024-07-17 21:30:46 +0400

    update RightPanel.tsx

--- src/features/Dashboard/rightPanel/RightPanel.tsx
+++ src/features/Dashboard/rightPanel/RightPanel.tsx
@@ -2,10 +2,15 @@ import { useLocation, useParams } from "react-router-dom";
 import GroupInfo from "./groupInfo/GroupInfo";
 import Connections from "./connections/Connections";
 import PostCommentsAside from "./postComments/PostCommentsAside";
+import { useEffect } from "react";
+import { fetchTopUsers } from "../../../slices/user/userDataThunks";
+import { useAppDispatch } from "../../../hooks/reduxHooks";
 
 const RightPanel = () => {
   const location = useLocation();
   const { postId } = useParams<{ postId: string }>();
+
+  const dispatch = useAppDispatch();
 
   const isSingleCommunityPage = location.pathname.startsWith("/community");
   const isPostCommentsPage = location.pathname.startsWith("/post");
@@ -14,6 +19,11 @@ const RightPanel = () => {
     : isPostCommentsPage
     ? postId
     : "";
+
+  // Fetch top users only once when the component mounts
+  useEffect(() => {
+    dispatch(fetchTopUsers());
+  }, [dispatch]);
 
   return (
     <div className="bg-white col-span-1 h-[85vh] sticky top-[5rem] border rounded-md">
