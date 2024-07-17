e8c513d6f9469114c07e8aee990fb607297e3c31
Sandro <vipsandro11@gmail.com>
2024-07-17 21:31:02 +0400

    update userDataThunks.ts

--- src/slices/user/userDataThunks.ts
+++ src/slices/user/userDataThunks.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { database } from "../../config/firebase";
-import { ref, update } from "firebase/database";
+import { get, ref, update } from "firebase/database";
+import { TopUserTypes } from "./userTypes";
 
 // Async thunk to update user's bio, location, and interests in Firebase
 export const updateUserProfile = createAsyncThunk(
@@ -28,3 +29,41 @@ export const updateUserProfile = createAsyncThunk(
     }
   }
 );
+
+//fetch popular users
+
+export const fetchTopUsers = createAsyncThunk<
+  TopUserTypes[],
+  void,
+  { rejectValue: string }
+>("users/fetchTopUsers", async (_, { rejectWithValue }) => {
+  try {
+    const usersRef = ref(database, "users");
+    const snapshot = await get(usersRef);
+
+    if (!snapshot.exists()) {
+      throw new Error("No users found");
+    }
+
+    const users: TopUserTypes[] = [];
+    snapshot.forEach((childSnapshot) => {
+      const data = childSnapshot.val();
+      const followersCount = data.followers?.length || 0;
+
+      users.push({
+        id: data.uid,
+        followersCount,
+        name: data.name,
+      });
+
+      if (users.length >= 5) {
+        return true; // Stop iterating after collecting 5 users
+      }
+    });
+
+    return users;
+  } catch (error: any) {
+    console.error("Failed to fetch top users:", error.message || error);
+    return rejectWithValue(error.message || "Failed to fetch top users");
+  }
+});
