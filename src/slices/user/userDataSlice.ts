858b175d3238061e2f3fd39265d1597ad002b28a
Sandro <vipsandro11@gmail.com>
2024-07-18 17:35:42 +0400

    update userDataSlice.ts

--- src/slices/user/userDataSlice.ts
+++ src/slices/user/userDataSlice.ts
@@ -1,31 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { fetchTopUsers, updateUserProfile } from "./userDataThunks";
-import { TopUserTypes } from "./userTypes";
-// import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  fetchTopUsers,
+  fetchUserById,
+  updateUserProfile,
+} from "./userDataThunks";
+import { TopUserTypes, UserType } from "./userTypes";
 // import type { RootState } from "../store";
 
 interface UserDataState {
   loading: {
     updatingUserProfile: boolean;
     fetchingTopUsers: boolean;
+    fetchingSingleUser: boolean;
   };
   error: {
     updateUserProfile: string | null;
     fetchTopUsers: string | null;
+    fetchSingleUserError: string | null;
   };
   popularUsers: TopUserTypes[] | null;
+  singleUser: UserType | null;
 }
 
 const initialState: UserDataState = {
   loading: {
     updatingUserProfile: false,
     fetchingTopUsers: false,
+    fetchingSingleUser: false,
   },
   error: {
     updateUserProfile: null,
     fetchTopUsers: null,
+    fetchSingleUserError: null,
   },
   popularUsers: null,
+  singleUser: null,
 };
 
 export const userDataSlice = createSlice({
@@ -51,14 +60,30 @@ export const userDataSlice = createSlice({
       .addCase(fetchTopUsers.pending, (state) => {
         state.loading.fetchingTopUsers = true;
       })
-      .addCase(fetchTopUsers.fulfilled, (state, action) => {
-        state.loading.fetchingTopUsers = false;
-        state.popularUsers = action.payload;
-      })
+      .addCase(
+        fetchTopUsers.fulfilled,
+        (state, action: PayloadAction<TopUserTypes[]>) => {
+          state.loading.fetchingTopUsers = false;
+          state.popularUsers = action.payload;
+        }
+      )
       .addCase(fetchTopUsers.rejected, (state, action) => {
         state.loading.fetchingTopUsers = false;
         state.error.fetchTopUsers = action.payload || "unknown error";
       });
+    builder.addCase(fetchUserById.pending, (state) => {
+      state.loading.fetchingSingleUser = true;
+    });
+    builder.addCase(
+      fetchUserById.fulfilled,
+      (state, action: PayloadAction<UserType>) => {
+        state.loading.fetchingSingleUser = false;
+        state.singleUser = action.payload;
+      }
+    );
+    builder.addCase(fetchUserById.rejected, (state, action) => {
+      state.error.fetchTopUsers = action.payload || "unknown error";
+    });
   },
 });
 
