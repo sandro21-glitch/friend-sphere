affdcd42a729ac4c6407a5fcfb35ab4d7c9e0b25
Sandro <vipsandro11@gmail.com>
2024-07-19 18:42:26 +0400

    update UserInfo.tsx

--- src/features/Dashboard/userPage/userInfo/UserInfo.tsx
+++ src/features/Dashboard/userPage/userInfo/UserInfo.tsx
@@ -1,18 +1,49 @@
-import { MdUpdate } from "react-icons/md";
+import { MdGroups, MdUpdate } from "react-icons/md";
 import { UserType } from "../../../../slices/user/userTypes";
 import { SlUserFollowing } from "react-icons/sl";
+import { useAppSelector } from "../../../../hooks/reduxHooks";
 
 type UserInfoTypes = {
   singleUser: UserType;
 };
+
 const UserInfo = ({ singleUser }: UserInfoTypes) => {
-  const { followers, following, joinedGroups, registeredDate, bio, interests } =
-    singleUser;
+  const currUserGroups =
+    useAppSelector((store) => store?.auth?.userData?.joinedGroups) || [];
+
+  const {
+    followers,
+    following,
+    joinedGroups,
+    registeredDate,
+    bio,
+    interests,
+    uid,
+  } = singleUser;
+
+  // Ensure both joinedGroups and currUserGroups are arrays
+  const commonGroups = joinedGroups.filter((group) =>
+    currUserGroups.some((currGroup) => currGroup.groupId === group.groupId)
+  );
+
+  const displayGroups = commonGroups.slice(0, 3);
+  const remainingGroupsCount = commonGroups.length - displayGroups.length;
+
+  const formattedGroups = displayGroups
+    .map((commonG) => commonG.groupName)
+    .join(", ");
+
   return (
     <ul className="flex flex-col items-start w-full">
       <li className="flex items-center gap-2 text-md">
         <MdUpdate />
         {registeredDate}
+      </li>
+      <li className="flex items-center gap-2 text-md ">
+        <MdGroups />
+        {joinedGroups.length > 0
+          ? `${joinedGroups.length} community`
+          : "Not a member of any communities"}
       </li>
       <li className="flex items-center gap-2 text-md">
         <SlUserFollowing />
@@ -21,6 +52,21 @@ const UserInfo = ({ singleUser }: UserInfoTypes) => {
       <li className="flex items-center gap-2 text-md font-semibold">
         {followers ? `${followers.length} followers` : "0 followers"}
       </li>
+      {commonGroups.length > 0 && (
+        <li className="flex items-center gap-2 text-md">
+          You both are members of{" "}
+          <span className="font-bold text-deep-blue cursor-pointer">
+            {formattedGroups}
+            {remainingGroupsCount > 0 && (
+              <span className="font-medium text-gray-500">
+                {" and "}
+                {remainingGroupsCount} other
+                {remainingGroupsCount > 1 ? "s" : ""}
+              </span>
+            )}
+          </span>
+        </li>
+      )}
     </ul>
   );
 };
