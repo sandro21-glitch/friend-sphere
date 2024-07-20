6bcaf2f46a44bdb5428b8fc253b0cc64ecdc38c1
Sandro <vipsandro11@gmail.com>
2024-07-20 21:54:35 +0400

    update UserPageHeader.tsx

--- src/features/Dashboard/userPage/userPageContent/UserPageHeader.tsx
+++ src/features/Dashboard/userPage/userPageContent/UserPageHeader.tsx
@@ -26,7 +26,7 @@ const UserPageHeader = ({ name, location }: UserPageHeaderTypes) => {
       <div className="flex items-center flex-col">
         <h3 className="text-lg font-semibold">{name}</h3>
         <h6 className="flex items-center text-gray-500">
-          <CiLocationOn /> {location}
+          <CiLocationOn /> {location ? location : <span className="text-sm">N/A</span>}
         </h6>
       </div>
     </div>
