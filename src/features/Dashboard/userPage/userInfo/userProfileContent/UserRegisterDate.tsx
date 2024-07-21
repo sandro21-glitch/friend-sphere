d4bfb400c97d7090b0f2d846198003c531d28d13
Sandro <vipsandro11@gmail.com>
2024-07-21 22:25:32 +0400

    update UserRegisterDate.tsx

--- src/features/Dashboard/userPage/userInfo/userProfileContent/UserRegisterDate.tsx
+++ src/features/Dashboard/userPage/userInfo/userProfileContent/UserRegisterDate.tsx
@@ -1,12 +1,19 @@
 import { MdUpdate } from "react-icons/md";
+import { format } from "date-fns";
 
 type UserRegisterDateTypes = { registeredDate: string };
 
 const UserRegisterDate = ({ registeredDate }: UserRegisterDateTypes) => {
+  // Convert ISO date string to Date object
+  const date = new Date(registeredDate);
+
+  // Format date to "MMM d, yyyy"
+  const formattedDate = format(date, "MMM d, yyyy");
+
   return (
     <li className="flex items-center gap-2 text-md">
       <MdUpdate />
-      {registeredDate}
+      Joined on {formattedDate}
     </li>
   );
 };
