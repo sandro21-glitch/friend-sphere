523d7c4ba8f0e5ac020aef05ec5d2be827a6beac
Sandro <vipsandro11@gmail.com>
2024-07-21 22:06:35 +0400

    update SingleComunityPage.tsx

--- src/features/Dashboard/singleCommunityFeed/SingleComunityPage.tsx
+++ src/features/Dashboard/singleCommunityFeed/SingleComunityPage.tsx
@@ -1,31 +1,43 @@
-import { useLocation } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
-import GroupHeader from "./groupHeaderSection/GroupHeader";
-import { useEffect, useState } from "react";
-import GroupPostForm from "./groupHeaderSection/GroupPostForm";
-import { fetchCommunityPosts } from "../../../slices/posts/postThunks";
-import GroupPosts from "./groupPosts/GroupPosts";
-import PageLoader from "../../../ui/PageLoader";
-import DashboardPage from "../../../ui/DashboardPage";
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHooks';
+import GroupHeader from './groupHeaderSection/GroupHeader';
+import GroupPostForm from './groupHeaderSection/GroupPostForm';
+import { fetchCommunityPosts } from '../../../slices/posts/postThunks';
+import GroupPosts from './groupPosts/GroupPosts';
+import PageLoader from '../../../ui/PageLoader';
+import DashboardPage from '../../../ui/DashboardPage';
 
-const SingleComunityPage = () => {
+const SingleCommunityPage: React.FC = () => {
   const {
     loading: { fetching },
     error: { fetchingError },
   } = useAppSelector((store) => store.posts);
-  const [postPage, setPostPage] = useState("all");
+
+  const [postPage, setPostPage] = useState<string>('all');
+
   const location = useLocation();
-  const { id } = location.state;
+  const { id } = location.state || {};
+  
   const communityData = useAppSelector((store) =>
     store.communities.communityData?.find((data) => data.uid === id)
   );
+
   const dispatch = useAppDispatch();
-  if (!communityData) return null;
-  const { uid } = communityData;
 
   useEffect(() => {
-    dispatch(fetchCommunityPosts({ communityId: communityData.uid }));
-  }, [dispatch, postPage, uid]);
+    if (communityData) {
+      dispatch(fetchCommunityPosts({ communityId: communityData.uid }));
+    }
+  }, [dispatch, postPage, communityData]);
+
+  if (!id) {
+    return (
+      <section className="col-span-2 min-h-full h-full mt-5 bg-white">
+        {"Community ID not found in location state."}
+      </section>
+    );
+  }
 
   if (fetching) {
     return (
@@ -34,19 +46,30 @@ const SingleComunityPage = () => {
       </section>
     );
   }
-  if (fetchingError)
+
+  if (fetchingError) {
     return (
       <section className="col-span-2 min-h-full h-full mt-5 bg-white">
         {fetchingError || "Something went wrong"}
       </section>
     );
+  }
+
+  if (!communityData) {
+    return (
+      <section className="col-span-2 min-h-full h-full mt-5 bg-white">
+        {"Community data not found."}
+      </section>
+    );
+  }
+
   return (
-    <DashboardPage> 
+    <DashboardPage>
       <GroupHeader postPage={postPage} setPostPage={setPostPage} />
-      <GroupPostForm groupId={uid} />
+      <GroupPostForm groupId={communityData.uid} />
       <GroupPosts communityId={communityData.uid} />
     </DashboardPage>
   );
 };
 
-export default SingleComunityPage;
+export default SingleCommunityPage;
