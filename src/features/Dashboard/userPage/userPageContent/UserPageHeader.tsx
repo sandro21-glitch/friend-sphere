fc7d22aa4387a4ddcc27bffa222d6d8135fc02ce
Sandro <vipsandro11@gmail.com>
2024-07-26 17:14:20 +0400

    update UserPageHeader.tsx

--- src/features/Dashboard/userPage/userPageContent/UserPageHeader.tsx
+++ src/features/Dashboard/userPage/userPageContent/UserPageHeader.tsx
@@ -4,13 +4,14 @@ import UserIntroduction from "./UserIntroduction";
 type UserPageHeaderTypes = {
   name: string;
   location: string;
+  bio: string;
 };
 
-const UserPageHeader = ({ name, location }: UserPageHeaderTypes) => {
+const UserPageHeader = ({ name, location, bio }: UserPageHeaderTypes) => {
   return (
     <div className="flex flex-col items-center mb-10">
       <UserImageWithFollow name={name} />
-      <UserIntroduction location={location} name={name} />
+      <UserIntroduction location={location} name={name} bio={bio} />
     </div>
   );
 };
