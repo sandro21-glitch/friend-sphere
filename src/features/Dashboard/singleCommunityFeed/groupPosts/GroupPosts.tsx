d1c121f75184da21882fbfa8cfb20a2cc9f4dec0
Sandro <vipsandro11@gmail.com>
2024-07-27 18:53:45 +0400

    update GroupPosts.tsx

--- src/features/Dashboard/singleCommunityFeed/groupPosts/GroupPosts.tsx
+++ src/features/Dashboard/singleCommunityFeed/groupPosts/GroupPosts.tsx
@@ -1,37 +1,69 @@
-
-import { PostType } from "../../../../slices/community/communityTypes";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/reduxHooks";
 import SingleGroupPost from "./SingleGroupPost";
+import { fetchCommunityPosts } from "../../../../slices/posts/postThunks";
 
 type GroupPostTypes = {
   communityId: string;
-  posts: PostType[];
+  offset: string | undefined;
+  setOffset: (offset: string) => void;
 };
-const GroupPosts = ({ posts, communityId }: GroupPostTypes) => {
-  if (!posts || posts.length === 0) {
+
+const GroupPosts = ({ communityId, offset, setOffset }: GroupPostTypes) => {
+  const {
+    communityPosts: posts,
+    loading: { fetching },
+  } = useAppSelector((store) => store.posts);
+  
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (communityId) {
+      dispatch(fetchCommunityPosts({ communityId, offset: undefined, limit: 10 }));
+    }
+  }, [dispatch, communityId]);
+
+  const loadMorePosts = () => {
+    if (offset) {
+      dispatch(fetchCommunityPosts({ communityId, offset, limit: 10 }))
+        .unwrap()
+        .then((fetchedPosts) => {
+          if (fetchedPosts.length > 0) {
+            setOffset(fetchedPosts[fetchedPosts.length - 1].postId); // Update offset
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching posts:", error);
+        });
+    }
+  };
+
+  if (fetching && !posts) {
     return (
       <div className="py-5 px-2 text-center">
-        <p className="text-gray-500 italic">
-          No posts available. Be the first to post!
-        </p>
+        <p className="text-gray-500 italic">Loading posts...</p>
       </div>
     );
   }
+
+  if (!posts || posts.length === 0) {
+    return (
+      <div className="py-5 px-2 text-center">
+        <p className="text-gray-500 italic">No posts available. Be the first to post!</p>
+      </div>
+    );
+  }
+
   return (
     <div className="py-5 px-2">
-      <ul className="flex flex-col gap-5 ">
-        {posts
-          .slice()
-          .reverse()
-          .map((post) => {
-            return (
-              <SingleGroupPost
-                key={post.postId}
-                post={post}
-                communityId={communityId}
-              />
-            );
-          })}
+      <ul className="flex flex-col gap-5">
+        {posts.map((post) => (
+          <SingleGroupPost key={post.postId} post={post} communityId={communityId} />
+        ))}
       </ul>
+      <button type="button" onClick={loadMorePosts}>
+        {fetching ? "Loading..." : "Load more posts"}
+      </button>
     </div>
   );
 };
