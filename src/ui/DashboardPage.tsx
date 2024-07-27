924447d02a3a25b721880531562fb43459da7542
Sandro <vipsandro11@gmail.com>
2024-07-27 21:28:06 +0400

    update DashboardPage.tsx

--- src/ui/DashboardPage.tsx
+++ src/ui/DashboardPage.tsx
@@ -7,7 +7,7 @@ type DashboardPageTypes = {
 const DashboardPage = ({ children }: DashboardPageTypes) => {
   return (
     <section className="col-span-2 min-h-[85vh] w-full h-full mt-5 bg-white rounded-lg border">
-     {children}
+      {children}
     </section>
   );
 };
