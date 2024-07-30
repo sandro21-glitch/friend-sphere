2ed9671507cb43d6d6441615edde711919fc30df
Sandro <vipsandro11@gmail.com>
2024-07-30 19:53:11 +0400

    update SavedPostText.tsx

--- src/features/Dashboard/saved/singleSavedPost/SavedPostText.tsx
+++ src/features/Dashboard/saved/singleSavedPost/SavedPostText.tsx
@@ -1,8 +1,23 @@
 type SavedPostTextTypes = {
   userPost: string;
 };
+
 const SavedPostText = ({ userPost }: SavedPostTextTypes) => {
-  return <p className="mb-5">{userPost}</p>;
+  const parsePost = (post: string) => {
+    const parts = post.split(/(\s+)/).map((part, index) => {
+      if (part.startsWith("#")) {
+        return (
+          <span key={index} className="text-azure-blue font-bold">
+            {part}
+          </span>
+        );
+      }
+      return part;
+    });
+    return parts;
+  };
+
+  return <p className="mb-5">{parsePost(userPost)}</p>;
 };
 
 export default SavedPostText;
