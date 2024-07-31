79ac4af0f7fe102cd0bc1a493500aa46ec52e5ac
Sandro <vipsandro11@gmail.com>
2024-07-31 22:05:14 +0400

    update PageDataLoader.tsx

--- src/ui/PageDataLoader.tsx
+++ src/ui/PageDataLoader.tsx
@@ -1,13 +1,12 @@
-import DashboardPage from "./DashboardPage";
 import PageLoader from "./PageLoader";
 
 const PageDataLoader = () => {
   return (
-    <DashboardPage>
+    <section className="col-span-2 min-h-[85vh] w-full mt-5 bg-white">
       <div className="flex items-center justify-center h-full">
         <PageLoader />
       </div>
-    </DashboardPage>
+    </section>
   );
 };
 
