8d2f505070f79b3a8a2d9a6966fc21c7bb801c46
Sandro <vipsandro11@gmail.com>
2024-08-01 15:41:51 +0400

    feat: create UserProfileLink.tsx

---
+++ src/features/Dashboard/rightPanel/connections/popularUsers/singlePopularUser/UserProfileLink.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+type UserProfileLinkTypes = {
+  id: string;
+  name: string;
+  followersCount: number;
+};
+
+const UserProfileLink = ({
+  followersCount,
+  id,
+  name,
+}: UserProfileLinkTypes) => {
+  return (
+    <Link to={`user/${id}`} className="flex items-center gap-2">
+      <img
+        src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
+        alt=""
+        className="w-[2rem] h-[2rem]"
+      />
+      <div>
+        <div className="text-[16px] font-medium">{name}</div>
+        <div className="text-[12px] capitalize text-gray-500">
+          followers: {followersCount}
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default UserProfileLink;
