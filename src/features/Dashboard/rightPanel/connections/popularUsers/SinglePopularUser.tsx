30f1321dfcfe0a02a776fe396ed693442b4d15fd
Sandro <vipsandro11@gmail.com>
2024-08-01 13:38:17 +0400

    update SinglePopularUser.tsx

--- src/features/Dashboard/rightPanel/connections/popularUsers/SinglePopularUser.tsx
+++ src/features/Dashboard/rightPanel/connections/popularUsers/SinglePopularUser.tsx
@@ -1,20 +1,58 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { TopUserTypes } from "../../../../../slices/user/userTypes";
 import ConnectButton from "../../../../../ui/ConnectButton";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../../../../hooks/reduxHooks";
+import { followUser } from "../../../../../slices/user/userDataThunks";
+import { useState } from "react";
 
 type SinglePopularUserTypes = {
   user: TopUserTypes;
 };
+
 const SinglePopularUser = ({ user }: SinglePopularUserTypes) => {
   const { followersCount, name, id } = user;
+  const { uid: currentUserId, name: currentUserName } =
+    useAppSelector((store) => store.auth.userData) || {};
+  const {
+    error: { followingError },
+  } = useAppSelector((store) => store.userData);
+
+  const [loading, setLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleFollowUser = async () => {
+    if (currentUserId && currentUserName) {
+      setLoading(true);
+      try {
+        await dispatch(
+          followUser({
+            currentUserId,
+            currentUserName,
+            followUserId: id,
+            followUserName: name,
+          })
+        ).unwrap();
+      } catch (error) {
+        console.log(followingError || "failed to follow user");
+        alert(followingError || "failed to follow user");
+      } finally {
+        setLoading(false);
+        navigate(`user/${id}`);
+      }
+    }
+  };
+
   return (
     <li>
-      <Link
-        to={`user/${id}`}
-        className="flex justify-between items-center border border-slate-100  py-[.25rem] px-[.5rem]
+      <div
+        className="flex justify-between items-center border border-slate-100 py-[.25rem] px-[.5rem]
      rounded-md shadow-sm shadow-[#f2f5fc]"
       >
-        <div className="flex items-center gap-2">
+        <Link to={`user/${id}`} className="flex items-center gap-2">
           <img
             src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
             alt=""
@@ -26,9 +64,14 @@ const SinglePopularUser = ({ user }: SinglePopularUserTypes) => {
               followers: {followersCount}
             </div>
           </div>
-        </div>
-        <ConnectButton name="follow" follow />
-      </Link>
+        </Link>
+        <ConnectButton
+          name="follow"
+          follow
+          onClick={handleFollowUser}
+          loading={loading}
+        />
+      </div>
     </li>
   );
 };
