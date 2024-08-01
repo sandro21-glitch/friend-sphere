453d8da70addc770914b7326bc6dd95990fbcf65
Sandro <vipsandro11@gmail.com>
2024-08-01 16:42:43 +0400

    update UserImageWithFollow.tsx

--- src/features/Dashboard/userPage/userPageContent/UserImageWithFollow.tsx
+++ src/features/Dashboard/userPage/userPageContent/UserImageWithFollow.tsx
@@ -1,11 +1,20 @@
-import { SlUserFollow } from "react-icons/sl";
+import { SlUserFollow, SlUserUnfollow } from "react-icons/sl";
 import Tooltip from "../../../../ui/Tooltip";
+import { useAppSelector } from "../../../../hooks/reduxHooks";
 
 type UserImageWithFollowProps = {
   name: string;
+  uid: string;
 };
 
-const UserImageWithFollow = ({name}:UserImageWithFollowProps) => {
+const UserImageWithFollow = ({ name, uid }: UserImageWithFollowProps) => {
+  const { uid: currUserUid, following = [] } =
+    useAppSelector((store) => store.auth.userData) || {};
+
+  const isFollowing = following
+    ? Object.values(following).some((follow) => follow.userUid === uid)
+    : false;
+
   return (
     <div className="relative w-fit mb-5">
       <img
@@ -13,11 +22,19 @@ const UserImageWithFollow = ({name}:UserImageWithFollowProps) => {
         alt="user image"
         className="w-20 h-20"
       />
-      <span className="absolute bottom-0 -right-3 border border-azure-blue w-8 h-8 rounded-full cursor-pointer flex items-center justify-center group">
-        <SlUserFollow className="text-azure-blue" />
-        {/* Tooltip */}
-        <Tooltip action="follow" message={name} />
-      </span>
+      {isFollowing ? (
+        <span className="absolute bottom-0 -right-3 border border-red-600 w-8 h-8 rounded-full cursor-pointer flex items-center justify-center group">
+          <SlUserUnfollow className="text-red-600" />
+          {/* Tooltip */}
+          <Tooltip action="unfollow" message={name} />
+        </span>
+      ) : (
+        <span className="absolute bottom-0 -right-3 border border-azure-blue w-8 h-8 rounded-full cursor-pointer flex items-center justify-center group">
+          <SlUserFollow className="text-azure-blue" />
+          {/* Tooltip */}
+          <Tooltip action="follow" message={name} />
+        </span>
+      )}
     </div>
   );
 };
