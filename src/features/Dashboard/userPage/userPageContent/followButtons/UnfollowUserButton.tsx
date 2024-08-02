185c5353cf7ff492c573d99cb4de4c7774db8bee
Sandro <vipsandro11@gmail.com>
2024-08-02 20:12:45 +0400

    update UnfollowUserButton.tsx

--- src/features/Dashboard/userPage/userPageContent/followButtons/UnfollowUserButton.tsx
+++ src/features/Dashboard/userPage/userPageContent/followButtons/UnfollowUserButton.tsx
@@ -1,22 +1,46 @@
-import { SlUserFollow } from "react-icons/sl";
+import { SlUserUnfollow } from "react-icons/sl";
 import Tooltip from "../../../../../ui/Tooltip";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../../../../hooks/reduxHooks";
+import { unfollowUser } from "../../../../../slices/user/userDataThunks";
 
-type UnfollowUserButtonTypes = {
+type FollowUserButtonTypes = {
   name: string;
   uid: string;
 };
 
-const UnfollowUserButton = ({ name, uid }: UnfollowUserButtonTypes) => {
-  console.log(uid);
+const UnfollowUserButton = ({ name, uid }: FollowUserButtonTypes) => {
+  const currentUserId = useAppSelector((store) => store.auth.userData?.uid);
+  const unFollowingError = useAppSelector(
+    (store) => store.userData.error?.unFollowingError
+  );
+
+  const dispatch = useAppDispatch();
+
+  const handleUnfollowUser = async () => {
+    if (uid && currentUserId) {
+      try {
+        await dispatch(
+          unfollowUser({ currentUserId, unfollowUserId: uid })
+        ).unwrap();
+      } catch (error) {
+        console.log(unFollowingError || "failed to unfollow user");
+        alert(unFollowingError || "failed to unfollow user");
+      }
+    }
+  };
 
   return (
     <button
       type="button"
-      className="absolute bottom-0 -right-3 border border-azure-blue w-8 h-8 rounded-full cursor-pointer flex items-center justify-center group"
+      onClick={handleUnfollowUser}
+      className="absolute bottom-0 -right-3 border border-red-600 w-8 h-8 rounded-full cursor-pointer flex items-center justify-center group"
     >
-      <SlUserFollow className="text-azure-blue" />
+      <SlUserUnfollow className="text-red-600" />
       {/* Tooltip */}
-      <Tooltip action="follow" message={name} />
+      <Tooltip action="unfollow" message={name} />
     </button>
   );
 };
