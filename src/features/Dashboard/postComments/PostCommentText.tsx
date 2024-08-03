d40a78ffa2d0d803f71002036ed166b13d0c8a5f
Sandro <vipsandro11@gmail.com>
2024-08-03 22:58:42 +0400

    update PostCommentText.tsx

--- src/features/Dashboard/postComments/PostCommentText.tsx
+++ src/features/Dashboard/postComments/PostCommentText.tsx
@@ -3,7 +3,21 @@ type PostTextTypes = {
 };
 
 const PostCommentText = ({ userPost }: PostTextTypes) => {
-  return <p className="mb-5">{userPost}</p>;
+  const parsePost = (post: string) => {
+    const parts = post.split(/(\s+)/).map((part, index) => {
+      if (part.startsWith("#")) {
+        return (
+          <span key={index} className="text-azure-blue font-bold">
+            {part}
+          </span>
+        );
+      }
+      return part;
+    });
+    return parts;
+  };
+
+  return <p className="mb-5">{parsePost(userPost)}</p>;
 };
 
 export default PostCommentText;
