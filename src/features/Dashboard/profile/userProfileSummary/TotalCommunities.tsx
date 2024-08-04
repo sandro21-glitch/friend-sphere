222ba974a3a878c3766bfa570e98ac3de73cf8ff
Sandro <vipsandro11@gmail.com>
2024-08-04 14:29:36 +0400

    update TotalCommunities.tsx

--- src/features/Dashboard/profile/userProfileSummary/TotalCommunities.tsx
+++ src/features/Dashboard/profile/userProfileSummary/TotalCommunities.tsx
@@ -1,14 +1,14 @@
 import { useAppSelector } from "../../../../hooks/reduxHooks";
 
 const TotalCommunities = () => {
-  const communityCount = useAppSelector(
-    (store) => store.communities.fullGroupList?.length || 0
+  const totalUserCommunities = useAppSelector(
+    (store) => store.auth.userData?.joinedGroups.length || 0
   );
 
   return (
     <div className="flex justify-between items-center text-[14px]">
       <p className="text-gray-500">Total Communities</p>
-      <p>{communityCount}</p>
+      <p>{totalUserCommunities}</p>
     </div>
   );
 };
