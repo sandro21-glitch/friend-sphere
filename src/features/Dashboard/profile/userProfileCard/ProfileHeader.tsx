29ea5d91d9b46f5d34c5a1deb5dca0b9507e714d
Sandro <vipsandro11@gmail.com>
2024-08-04 15:06:54 +0400

    update ProfileHeader.tsx

--- src/features/Dashboard/profile/userProfileCard/ProfileHeader.tsx
+++ src/features/Dashboard/profile/userProfileCard/ProfileHeader.tsx
@@ -1,8 +1,12 @@
+import React from "react";
 import { FaUserEdit } from "react-icons/fa";
 import { useAppSelector } from "../../../../hooks/reduxHooks";
+import { parseBioText } from "../../../../utils/TextUtils";
 
-const ProfileHeader = () => {
+const ProfileHeader: React.FC = () => {
   const { userData } = useAppSelector((store) => store.auth);
+
+  const bio = userData?.bio || "Bio not added";
 
   return (
     <div className="flex justify-center items-center flex-col mb-3">
@@ -11,11 +15,11 @@ const ProfileHeader = () => {
         alt="user image"
         className="w-[5.5rem] h-[5.5rem] mb-5"
       />
-      <div className=" flex items-center flex-col">
+      <div className="flex items-center flex-col">
         <h2 className="text-[1.1rem] mb-1 font-semibold">{userData?.name}</h2>
         <p className="text-[14px] text-black flex items-center gap-1">
           <FaUserEdit />
-          {userData?.bio || "Bio not added"}
+          {parseBioText(bio)}
         </p>
         <hr />
       </div>
