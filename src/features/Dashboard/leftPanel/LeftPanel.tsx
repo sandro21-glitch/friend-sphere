3c89669f6f4dac5cf0e8a9b0b883da58d00cf906
Sandro <vipsandro11@gmail.com>
2024-08-06 22:04:24 +0400

    update LeftPanel.tsx

--- src/features/Dashboard/leftPanel/LeftPanel.tsx
+++ src/features/Dashboard/leftPanel/LeftPanel.tsx
@@ -4,7 +4,7 @@ import PanelNavigation from "./navigation/PanelNavigation";
 const LeftPanel = () => {
   return (
     <div className="col-span-1 h-[85vh] sticky top-[5rem] border rounded-md py-5 bg-white">
-      <div className="p-5">
+      <div className="px-5">
         <PanelNavigation />
         <hr />
         <PanelCommunities />
