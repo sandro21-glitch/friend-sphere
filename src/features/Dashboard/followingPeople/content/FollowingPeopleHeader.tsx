65ac5e18370dc40c166667fe06a43f8ff2fa220c
Sandro <vipsandro11@gmail.com>
2024-08-06 20:53:13 +0400

    feat: create FollowingPeopleHeader.tsx

---
+++ src/features/Dashboard/followingPeople/content/FollowingPeopleHeader.tsx
@@ -0,0 +1,11 @@
+const FollowingPeopleHeader = () => {
+  return (
+    <header className=" border-b p-4">
+      <h3 className="text-[1rem] font-bold text-center">
+        People you're following
+      </h3>
+    </header>
+  );
+};
+
+export default FollowingPeopleHeader;
