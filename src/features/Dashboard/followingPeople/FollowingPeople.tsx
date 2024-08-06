1990076951917e862573107540d3d287711fd642
Sandro <vipsandro11@gmail.com>
2024-08-06 20:53:11 +0400

    feat: create FollowingPeople.tsx

---
+++ src/features/Dashboard/followingPeople/FollowingPeople.tsx
@@ -0,0 +1,14 @@
+import DashboardPage from "../../../ui/DashboardPage";
+import FollowingPeopleHeader from "./content/FollowingPeopleHeader";
+
+const FollowingPeople = () => {
+  return (
+    <DashboardPage>
+      <div>
+        <FollowingPeopleHeader />
+      </div>
+    </DashboardPage>
+  );
+};
+
+export default FollowingPeople;
