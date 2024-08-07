59d65650247c490f4a830a392693379fee8a0d76
Sandro <vipsandro11@gmail.com>
2024-08-07 18:51:02 +0400

    update SinglePostComment.tsx

--- src/features/Dashboard/rightPanel/postComments/SinglePostComment.tsx
+++ src/features/Dashboard/rightPanel/postComments/SinglePostComment.tsx
@@ -1,5 +1,6 @@
 import { formatDistanceToNow, parseISO } from "date-fns";
 import SinglePostCommentTexts from "./SinglePostCommentTexts";
+import { Link } from "react-router-dom";
 
 type SingleCommentTypes = {
   comment: {
@@ -11,30 +12,28 @@ type SingleCommentTypes = {
 };
 
 const SinglePostComment = ({ comment }: SingleCommentTypes) => {
-  const { userComment, userName, postedAt } = comment || {};
+  const { userComment, userName, postedAt, userId } = comment || {};
   const parsedDate =
     postedAt && !isNaN(Date.parse(postedAt)) ? parseISO(postedAt) : new Date();
 
   // format the distance to now in a human-readable format
   const commentedAt = formatDistanceToNow(parsedDate, { addSuffix: true });
 
-
-
   return (
     <li className="border-b pb-2 mb-2">
-      <div className="flex items-start mb-2">
+      <Link to={`/user/${userId}`} className="flex items-start mb-2">
         <img
           src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
           alt="user avatar"
-          className="w-8 h-8 mr-2"
+          className="w-9 h-9 mr-2"
         />
-        <div className="leading-4">
+        <div className="leading-[18px]">
           <p className="text-[.9rem] font-medium">{userName}</p>
           <p className="text-[.7rem] text-gray-500">
             {commentedAt.replace("about", "")}
           </p>
         </div>
-      </div>
+      </Link>
       <SinglePostCommentTexts userComment={userComment} />
     </li>
   );
