d314e13e404f93e3deb0b12c9815f8d85a35408c
Sandro <vipsandro11@gmail.com>
2024-08-09 20:03:57 +0400

    update Connections.tsx

--- src/features/Dashboard/rightPanel/connections/Connections.tsx
+++ src/features/Dashboard/rightPanel/connections/Connections.tsx
@@ -3,7 +3,10 @@ import SuggestedCommunities from "./SuggestedCommunities";
 
 const Connections = () => {
   return (
-    <div className="p-5">
+    <div
+      className="p-5 hidden lg:block bg-white col-span-1 h-[85vh]
+     sticky top-[5rem] border rounded-md"
+    >
       <SuggestedCommunities />
       <hr />
       <PopularUsers />
