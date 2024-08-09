d4719ae43154ad12757c88b62b855859cab9053e
Sandro <vipsandro11@gmail.com>
2024-08-09 20:03:41 +0400

    update LeftPanel.tsx

--- src/features/Dashboard/leftPanel/LeftPanel.tsx
+++ src/features/Dashboard/leftPanel/LeftPanel.tsx
@@ -1,9 +1,16 @@
+import { useAppSelector } from "../../../hooks/reduxHooks";
 import PanelCommunities from "./communityNavList/PanelCommunities";
 import PanelNavigation from "./navigation/PanelNavigation";
 
 const LeftPanel = () => {
+  const { isNavOpen } = useAppSelector((store) => store.modals);
+
   return (
-    <div className="col-span-1 h-[85vh] sticky top-[5rem] border rounded-md py-5 bg-white">
+    <div
+      className={`col-span-1 h-[85vh] fixed z-[99999] w-[65%] sm:w-[50%]
+       lg:w-auto left-0 lg:sticky lg:left-auto top-[5rem]
+       border rounded-md py-5 bg-white ${isNavOpen ? "block" : "hidden"}`}
+    >
       <div className="px-5">
         <PanelNavigation />
         <hr />
