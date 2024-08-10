4d77e1b18445b3b2386cb371d751bec41335d08a
Sandro <vipsandro11@gmail.com>
2024-08-10 18:46:07 +0400

    update PostCommentsAside.tsx

--- src/features/Dashboard/rightPanel/postComments/PostCommentsAside.tsx
+++ src/features/Dashboard/rightPanel/postComments/PostCommentsAside.tsx
@@ -13,8 +13,8 @@ const PostCommentsAside = () => {
 
   return (
     <div
-      className="p-5 bg-white col-span-1 h-[85vh]
-     sticky top-[5rem] border rounded-md"
+      className="p-5 bg-white col-span-2 lg:col-span-1 h-[85vh]
+     sticky top-[5rem] border rounded-md "
     >
       <h4 className="border-b-2 text-[1rem] pb-2 text-center font-medium mb-5">
         Recent Comments
