f1f325de44fddd684f6474fa10d0e86663872810
Sandro <vipsandro11@gmail.com>
2024-08-10 18:56:20 +0400

    update SingleCommunityListItem.tsx

--- src/features/Dashboard/leftPanel/communityNavList/SingleCommunityListItem.tsx
+++ src/features/Dashboard/leftPanel/communityNavList/SingleCommunityListItem.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
+import { setIsNavOpen } from "../../../../slices/modals/modalSlice";
 
 type SingleCommunityListItemTypes = {
   item: {
@@ -8,9 +10,11 @@ type SingleCommunityListItemTypes = {
 };
 
 const SingleCommunityListItem = ({ item }: SingleCommunityListItemTypes) => {
+  const dispatch = useAppDispatch();
   return (
     <li
       key={item.uid}
+      onClick={() => dispatch(setIsNavOpen(false))}
       className="text-gray-600 hover:text-azure-blue text-[16px] transition-colors ease-in duration-150"
     >
       <Link to={`community/${item.name}`} state={{ id: item.uid }}>
