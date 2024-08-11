3b37a1ebaf332bb7ee180857a28add150e44e569
Sandro <vipsandro11@gmail.com>
2024-08-11 18:37:19 +0400

    update FormLayour.tsx

--- src/ui/FormLayour.tsx
+++ src/ui/FormLayour.tsx
@@ -4,7 +4,7 @@ type FormLayoutProps = {
 };
 const FormLayout = ({ children, onSubmit }: FormLayoutProps) => {
   return (
-    <section className="bg-white min-h-screen flex justify-center items-center">
+    <section className="bg-white min-h-screen flex justify-center items-center max-w-[90vw] mx-auto">
       <div className="flex items-center justify-center section-center w-full">
         <div className="w-full max-w-[28rem] ">
           <form className="w-full" onSubmit={onSubmit}>
