2cd606d18618f36fde706d4eebc89a66c84128ce
Sandro <vipsandro11@gmail.com>
2024-08-12 18:39:57 +0400

    update PageDataLoader.tsx

--- src/ui/PageDataLoader.tsx
+++ src/ui/PageDataLoader.tsx
@@ -2,11 +2,11 @@ import PageLoader from "./PageLoader";
 
 const PageDataLoader = () => {
   return (
-    <section className="col-span-2 min-h-[85vh] w-full mt-5 bg-white">
+    <article className="col-span-2 min-h-[85vh] w-full mt-5 bg-white">
       <div className="flex items-center justify-center h-full">
         <PageLoader />
       </div>
-    </section>
+    </article>
   );
 };
 
