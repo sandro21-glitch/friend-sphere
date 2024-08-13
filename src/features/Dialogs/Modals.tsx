d77635c5dbbb9033b241371ab5dbffc5a75f4f6d
Sandro <vipsandro11@gmail.com>
2024-08-13 13:45:50 +0400

    update Modals.tsx

--- src/features/Dialogs/Modals.tsx
+++ src/features/Dialogs/Modals.tsx
@@ -1,10 +1,17 @@
-import { useAppSelector } from "../../hooks/reduxHooks";
+import { useAppSelector, useAppDispatch } from "../../hooks/reduxHooks";
+import {
+  setDeletePostModal,
+  setJoinCommunityModal,
+  setLeaveCommunity,
+  setUpdateProfileModal,
+} from "../../slices/modals/modalSlice";
 import DeletePostPopup from "./deletePostPopup/DeletePostPopup";
 import EditProfilePopup from "./editProfilePopup/EditProfilePopup";
 import JoinCommunityPopup from "./joinCommunityPopup/JoinCommunityPopup";
 import LeaveGroupPopup from "./leaveGroup/LeaveGroupPopup";
 
 const Modals = () => {
+  const dispatch = useAppDispatch();
   const { updateProfileModal, joinCommunity, leaveCommunity, deletePostModal } =
     useAppSelector((store) => store.modals);
   const isAnyModalOpen =
@@ -13,10 +20,26 @@ const Modals = () => {
     leaveCommunity.isModalOpen ||
     deletePostModal.isModalOpen;
   if (!isAnyModalOpen) return null;
+
+  const handleCloseModals = () => {
+    if (updateProfileModal) dispatch(setUpdateProfileModal(false));
+    if (joinCommunity.isModalOpen)
+      dispatch(
+        setJoinCommunityModal({ communityData: null, isModalOpen: false })
+      );
+    if (leaveCommunity.isModalOpen)
+      dispatch(setLeaveCommunity({ dataIds: null, isModalOpen: false }));
+    if (deletePostModal.isModalOpen)
+      dispatch(setDeletePostModal({ dataIds: null, isModalOpen: false }));
+  };
+
   return (
     <div className="w-full h-full fixed inset-0 flex items-center justify-center z-[9999] min-h-screen">
-      <div className="fixed inset-0 bg-black opacity-30"></div>
-      <div className="z-[9999]">
+      <div
+        className="fixed inset-0 bg-black opacity-30"
+        onClick={handleCloseModals}
+      ></div>
+      <div className="z-[9999]" onClick={(e) => e.stopPropagation()}>
         {updateProfileModal ? (
           <EditProfilePopup />
         ) : joinCommunity.isModalOpen ? (
