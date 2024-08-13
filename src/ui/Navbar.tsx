1d6a9f26c26da3e1c37eb5061c9f0dbb8bf50ec3
Sandro <vipsandro11@gmail.com>
2024-08-13 15:43:50 +0400

    update Navbar.tsx

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,10 +1,13 @@
+import { useEffect, useState } from "react";
 import UserMenuPopup from "../features/Dialogs/menuPopup/UserMenuPopup";
+import SearchResultsPanel from "../features/Dialogs/searchPanel/SearchResultsPanel";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import { setUserProfileModal } from "../slices/modals/modalSlice";
 import HamburgerMenu from "./HamburgerMenu";
 import Input from "./Input";
 import Logo from "./Logo";
 import { FaUserCircle } from "react-icons/fa";
+import { searchAll } from "../slices/search/searchThunks";
 const Navbar = () => {
   const { userProfileModal } = useAppSelector((store) => store.modals);
   const dispatch = useAppDispatch();
@@ -12,18 +15,32 @@ const Navbar = () => {
     dispatch(setUserProfileModal(!userProfileModal));
   };
 
+  const { status } = useAppSelector((store) => store.search);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  useEffect(() => {
+    if (searchQuery) {
+      dispatch(searchAll(searchQuery));
+    }
+  }, [dispatch, searchQuery]);
+
   return (
     <header className="p-[.5rem] border-b bg-white sticky inset-0 z-[999999]">
       <nav className="section-center section-x flex items-center justify-between gap-10 relative">
         <Logo height="2.5rem" />
         <HamburgerMenu />
-        <Input
-          id="search"
-          name="search"
-          type="text"
-          placeholder="Search for people, posts or communities"
-          className="max-w-[660px] py-2 rounded-full text-[14px]"
-        />
+        <div className="w-full max-w-[660px] static md:relative">
+          <Input
+            id="search"
+            name="search"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search for people, posts or communities"
+            className="w-full py-2 rounded-full text-[14px]"
+          />
+          {searchQuery && status !== "loading" && <SearchResultsPanel />}
+        </div>
         <FaUserCircle
           className="min-w-7 min-h-7 cursor-pointer text-gray-400"
           onClick={handleOpenModal}
