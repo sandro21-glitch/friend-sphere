4b785bd7deaf07b6b27de0e348610d14d04badbf
Sandro <vipsandro11@gmail.com>
2024-08-13 13:50:46 +0400

    update ProfileSummary.tsx

--- src/features/Dashboard/profile/userProfileSummary/ProfileSummary.tsx
+++ src/features/Dashboard/profile/userProfileSummary/ProfileSummary.tsx
@@ -6,15 +6,7 @@ const ProfileSummary = () => {
   return (
     <div className="bg-white  border rounded-md p-5 flex flex-col gap-2">
       <SummaryDate />
-      <div className="flex justify-between items-center text-[14px]">
-        <p className="text-gray-500">Total Posts</p>
-        <p>3</p>
-      </div>
       <TotalCommunities />
-      <div className="flex justify-between items-center text-[14px]">
-        <p className="text-gray-500">Posts in Communities</p>
-        <p>3 in 3 communities</p>
-      </div>
       <UserFollowerStats />
     </div>
   );
