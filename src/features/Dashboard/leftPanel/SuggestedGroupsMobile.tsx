6ae5ee0b4f133136010cbe27bdf71cd861577fe1
Sandro <vipsandro11@gmail.com>
2024-08-13 13:45:48 +0400

    update SuggestedGroupsMobile.tsx

--- src/features/Dashboard/leftPanel/SuggestedGroupsMobile.tsx
+++ src/features/Dashboard/leftPanel/SuggestedGroupsMobile.tsx
@@ -1,10 +1,23 @@
 import { MdGroups } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../../hooks/reduxHooks";
+import { setIsNavOpen } from "../../../slices/modals/modalSlice";
 
 const SuggestedGroupsMobile = () => {
+  const dispatch = useAppDispatch();
+
+  const handleCloseNav = () => {
+    const currentWidth = window.innerWidth;
+
+    if (currentWidth < 1024) {
+      dispatch(setIsNavOpen(false));
+    }
+  };
+
   return (
     <Link
       to={"allgroup"}
+      onClick={handleCloseNav}
       className="flex items-center gap-2 text-[16px] text-azure-blue cursor-pointer"
     >
       <MdGroups className="text-black" />
