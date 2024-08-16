477b1807f567984e5b80ae706261b6198b52ebc5
Sandro <vipsandro11@gmail.com>
2024-08-16 18:57:14 +0400

    update UserImageWithFollow.tsx

--- src/features/Dashboard/userPage/userPageContent/UserImageWithFollow.tsx
+++ src/features/Dashboard/userPage/userPageContent/UserImageWithFollow.tsx
@@ -21,6 +21,7 @@ const UserImageWithFollow = ({ name, uid }: UserImageWithFollowProps) => {
         src="https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg"
         alt="user image"
         className="w-20 h-20"
+        loading="lazy"
       />
       {isFollowing ? (
         <UnfollowUserButton name={name} uid={uid} />
